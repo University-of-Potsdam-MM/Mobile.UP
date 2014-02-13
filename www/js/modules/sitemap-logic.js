39c801e79d171b1a2f4c12b72d2d7781732a19ea
Hendrik Geßner <HENGE01@gmail.com>
2014-02-13 20:28:32 +0100

    References #1474 - Only search results are shown on map. Some markers seem to stick, though.

--- www/js/modules/sitemap-logic.js
+++ www/js/modules/sitemap-logic.js
@@ -44,16 +44,51 @@ $(document).on( "pageinit", "#sitemaps", function() {
  */
 $(document).on("pageshow", "#sitemaps", function() {
 	initializeMap();
+	initializeFilter();
 });
 
 var coords = new google.maps.LatLng(52.39345677934452, 13.128039836883545);
 var map = undefined;
+
+function initializeFilter() {
+	$("#filterable-locations").filterable("option", "filterCallback", filterLocations);
+}
+
+function filterLocations(index, searchValue) {
+	var text = $(this).text();
+	var result = text.toLowerCase().indexOf(searchValue) === -1;
+	
+	if (searchValue) {
+		// Don't show all markers, only the matching one
+		for (var i = 0; i < allMarkers.length; i++) {
+			allMarkers[i].setMap(null);
+		}
+		
+		if (!result) {
+			var source = $("a", this);
+			var href = source.attr("href");
+			var index = parseInt(href.slice(1));
+			searchedMarkers[index].setMap(map);
+		}
+	} else {
+		// Show all markers
+		for (var i = 0; i < allMarkers.length; i++) {
+			allMarkers[i].setMap(map);
+			searchedMarkers[i].setMap(null);
+		}
+	}
+	
+	return result;
+}
 
 /**
  * initializes the map and draws all markers which are currently selected
  */
 function initializeMap() {
 	drawMap(coords);
+	markers = [];
+	allMarkers = [];
+	searchedMarkers = [];
 	
 	if($('#Mensen:checkbox').is(':checked')) {
 		drawMensen();
@@ -75,16 +110,7 @@ function drawMensen() {
 		};
 		
 	$.getJSON("js/geojson/mensen-griebnitzsee.json", function(data) {
-		gMensen = new GeoJSON(data, options);
-		if (gMensen.error) {
-			console.log(gMensen.error);
-		} else {
-			for(var i=0;i<gMensen.length;i++) {
-				gMensen[i].setMap(map);
-			}
-		}
-		
-		insertSearchableFeatureCollection(data);
+		insertSearchableFeatureCollection(options, data);
 	});
 }
 
@@ -94,16 +120,7 @@ function drawTerminals() {
 		};
 		
 	$.getJSON("js/geojson/terminals-griebnitzsee.json", function(data) {
-		gTerminals = new GeoJSON(data, options);
-		if (gTerminals.error) {
-			console.log(gTerminals.error);
-		} else {
-			for(var i=0;i<gTerminals.length;i++) {
-				gTerminals[i].setMap(map);
-			}
-		}
-		
-		insertSearchableFeatureCollection(data);
+		insertSearchableFeatureCollection(options, data);
 	});
 }
 
@@ -117,16 +134,7 @@ function drawInstitutes() {
 		};
 	
 	$.getJSON("js/geojson/institutes-griebnitzsee.json", function(data) {
-		gInstitutes = new GeoJSON(data, options);
-		if (gInstitutes.error) {
-			console.log(gInstitutes.error);
-		} else {
-			for(var i=0;i<gInstitutes.length;i++) {
-				gInstitutes[i].setMap(map);
-			}
-		}
-		
-		insertSearchableFeatureCollection(data);
+		insertSearchableFeatureCollection(options, data);
 	});
 }
 
@@ -143,11 +151,11 @@ function drawMap(latlng) {
         };
 	map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 	// Add an overlay to the map of current lat/lng
-	marker = new google.maps.Marker({
-		position: latlng,
-		title: "Greetings!"
-	});
-	marker.setMap(map);
+	//marker = new google.maps.Marker({
+	//	position: latlng,
+	//	title: "Greetings!"
+	//});
+	//marker.setMap(map);
 }
 
 function insertSearchables(searchables) {
@@ -195,18 +203,53 @@ $(document).on("click", "#filterable-locations a", function () {
 	}
 });
 
-function insertSearchableFeatureCollection(collection) {
+var markers;
+var allMarkers;
+var searchedMarkers;
+
+function insertSearchableFeatureCollection(options, collection) {
 	var items = _.map(collection.features, function(item) {
 		var result = {};
 		result.name = item.properties.Name;
 		result.description = item.properties.description;
 		
 		// Save item context
-		result.context = JSON.parse(JSON.stringify(collection));
-		result.context.features = [item];
+		var context = JSON.parse(JSON.stringify(collection));
+		context.features = [item];
+		
+		// Save marker and get its index
+		result.index = saveMarker(options, context);
 		
 		return result;
 	});
 	
 	insertSearchables(items);
+	insertMapsMarkers(items);
+}
+
+function insertMapsMarkers(items) {
+	for (var i in items) {
+		var m = loadMarker(items[i].index);
+		var gMarkers = new GeoJSON(m.context, m.options);
+		var tmpMarkers = new GeoJSON(m.context, m.options);
+		
+		if (gMarkers.error) {
+			console.log(gMarkers.error);
+		} else {
+			gMarkers[0].setMap(map);
+			tmpMarkers[0].setMap(null);
+			
+			allMarkers[items[i].index] = gMarkers[0];
+			searchedMarkers[items[i].index] = tmpMarkers[0];
+		}
+	}
+}
+
+function saveMarker(options, context) {
+	markers.push({options: options, context: context});
+	return markers.length - 1;
+}
+
+function loadMarker(index) {
+	return markers[index];
 }
