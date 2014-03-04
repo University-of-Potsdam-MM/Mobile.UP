c58f357d69ae80c843a5ffa6793689b78294dee7
Richard Metzler <richard.metzler@me.com>
2014-03-04 13:14:43 +0100

    einfuehrung backbone

--- www/js/modules/library-logic.js
+++ www/js/modules/library-logic.js
@@ -1,5 +1,125 @@
 // TODO: check if internet is available
 
+var App = {
+  model: {}, // model classes
+  collection: {}, // collection classes
+  collections: {}, //actual collections
+  view: {}, // view classes
+}
+
+App.model.Book = Backbone.Model.extend({
+  // Book instance properties
+  initialize: function () {
+    console.log('initialize', this.attributes);
+  }
+},{
+  // Book class properties
+
+  // TODO: create Backbone Model
+  // TODO: Standort Informationen am Model
+  fromXmlRecord: function(xmlRecord) {
+    // console.log('xml:', xmlRecord);
+    var $xmlRecord = $(xmlRecord);
+    var recordId = App.model.Book.textForTag(xmlRecord, 'recordIdentifier');
+    var model = {
+      id:        recordId,
+      recordId:  recordId,
+      title:     App.model.Book.textForTag(xmlRecord, 'title'),
+      subtitle:  App.model.Book.textForTag(xmlRecord, 'subTitle'),
+      abstract:  App.model.Book.textForTag(xmlRecord, 'abstract'),
+      toc:       App.model.Book.split_string(App.model.Book.textForTag(xmlRecord, 'tableOfContents'),'--'),
+      authors:   App.model.Book.authors($xmlRecord),
+      publisher: App.model.Book.textForTag(xmlRecord, 'publisher'),
+      isbn:      App.model.Book.textForQuery($xmlRecord, 'identifier[type=isbn]')
+    };
+    // console.log('model.toc', model.toc);
+    return new App.model.Book(model);
+  },
+
+  textForTag: function(node, tagName) {
+    var firstTagNode = node.getElementsByTagName(tagName)[0];
+    if (firstTagNode) {
+      return firstTagNode.textContent;
+    } else {
+      return null;
+    }
+  },
+
+  split_string: function(string, split_by){
+    if (string) {
+      return string.split(split_by);
+    }
+    return null;
+  },
+
+  textForQuery: function(jqNode, query){
+    return _.pluck(jqNode.find(query), 'textContent');
+  },
+
+  // TODO: a view logic that displays only some of the authors (eg: "Gamma et al.")
+  authors: function($recordData){
+    var nameNodes = $recordData.find('name[type=personal]')
+    var names = _.map(nameNodes, function(node){
+      var $node = $(node);
+      var author = [
+        App.model.Book.textForQuery($node, 'namePart[type=family]')[0],
+        App.model.Book.textForQuery($node, 'namePart[type=given]')[0]
+      ];
+      return author;
+    });
+    // console.log('names:',names);
+    return names;
+  }
+
+});
+// END Book
+
+
+App.collection.BookList = Backbone.Collection.extend({
+  model: App.model.Book,
+
+  // TODO: create object to communicate with SRU and provide pagination for query
+  // TODO: store numberOfRecords for search
+  addXmlSearchResult: function(xmlSearchResult){
+    // console.log('xml',xmlSearchResult);
+    var records = xmlSearchResult.getElementsByTagName('recordData');
+    // return _.map(records, book);
+    this.add ( _.map(records, App.model.Book.fromXmlRecord ) );
+
+    console.log('addXmlSearchResult', this.models);
+
+    return this;
+  },
+
+});
+
+App.model.LibrarySearch = Backbone.Model.extend({
+  initialize: function(query) {
+    this.set('query', query);
+  },
+  loadNext: function() {
+    // fetch the next 10 books
+  }
+});
+
+App.view.Search = Backbone.View.extend({
+  // TODO: this is the main Search Page with everything inside
+})
+
+App.view.LibrarySearchView = Backbone.View.extend({
+  events: {
+    "click .pagination" : this.loadNext,
+  },
+  loadNext: function(){
+    this.model.loadNext();
+  }
+});
+
+App.view.BookDetailView = Backbone.View.extend({});
+App.view.BookShortView = Backbone.View.extend({});
+
+///////////////////////
+
 var environment = 'development';
 
 // debugging controller
@@ -22,7 +142,9 @@ function registerEventSearch(){
 function registerEventChooseBook(){
   $( '#search-results > ul' ).on( 'click', 'li', function(ev){
     // TODO query Standortinfo for this record
-    renderDetailView(getRecord(this.id));
+    debugger
+    var book = App.collections.searchResults.get(this.id);
+    renderDetailView(book);
   } );
 }
 
@@ -31,13 +153,22 @@ function updateResults() {
   Q(clearSearch)
   .then(getKeyword)
   .then(loadSearch)
-  .then(xmlToBooksArray)
+  .then(addXmlSearchResult)
+  // TODO: adding those books should fire the add event which the view has to listen on
   .then(updateResultsList)
   .then(renderBookListView)
   .then(registerEventChooseBook)
   .catch(logError);
 }
 
+// this is a function i use to migrate to Backbone
+// TODO remove it
+function addXmlSearchResult(xmlSearchResult) {
+  var searchResults = new App.collection.BookList();
+  App.collections.searchResults = searchResults;
+  searchResults.addXmlSearchResult(xmlSearchResult);
+  return searchResults.models;
+};
 
 // controller / helper
 var logError = function (err) {
@@ -88,10 +219,10 @@ function updateResultsList(list) {
   return this.resultsList;
 }
 
-function getRecord(recordID){
-  // get book from list with recordID
-  return _.find(this.resultsList, function(book){ return book.recordID == recordID; });
-}
+// function getRecord(recordId){
+//   // get book from list with recordId
+//   return _.find(this.resultsList, function(book){ return book.recordId == recordId; });
+// }
 
 // TODO: look here for setting meta data / attributes on a Backbone collection
 // http://stackoverflow.com/a/5930838/104959
@@ -106,36 +237,36 @@ var LibrarySearch = function(query, options) {
     query:query,
     options: options
   }, {
-    baseURL:
-      function() {
-        if ('development' == environment){
-          return '/api/search';
-        } else {
-          return "http://sru.gbv.de";
-        }
-      },
-    url:
-      function() {
-        return this.baseURL() + '/opac-de-517?version=1.1&operation=searchRetrieve' +
-          '&query=' + this.query +
-          '&startRecord=' + this.options.startRecord +
-          '&maximumRecords=' + this.options.maximumRecords +
-          '&recordSchema=mods';
-      },
-    loadSearch:
-      function() {
-        // TODO: return Backbone collection instead of promise
-        var d = Q.defer();
-        var url = this.url();
-        $.get(url).done(d.resolve).fail(d.reject);
-        return d.promise;
-      },
-    next:
-      function(){
-        this.options.startRecord += this.options.maximumRecords;
-        // TODO: update Backbone Collection
-        return this.loadSearch();
+
+    baseURL: function() {
+      if ('development' == environment){
+        return '/api/search';
+      } else {
+        return "http://sru.gbv.de";
       }
+    },
+
+    url: function() {
+      return this.baseURL() + '/opac-de-517?version=1.1&operation=searchRetrieve' +
+        '&query=' + this.query +
+        '&startRecord=' + this.options.startRecord +
+        '&maximumRecords=' + this.options.maximumRecords +
+        '&recordSchema=mods';
+    },
+
+    loadSearch: function() {
+      // TODO: return and memorize Backbone collection instead of promise
+      var d = Q.defer();
+      var url = this.url();
+      $.get(url).done(d.resolve).fail(d.reject);
+      return d.promise;
+    },
+
+    next: function() {
+      this.options.startRecord += this.options.maximumRecords;
+      // TODO: update Backbone Collection
+      return this.loadSearch();
+    }
   });
 };
 
@@ -144,67 +275,10 @@ function loadSearch(queryString) {
     return query.loadSearch();
 }
 
-// TODO: create object to communicate with SRU and provide pagination for query
-// TODO: store numberOfRecords for search
-function xmlToBooksArray(xmlSearchResult){
-  // console.log('xml',xmlSearchResult);
-  var records = xmlSearchResult.getElementsByTagName('recordData');
-  return _.map(records, book);
-};
-
-// TODO: create Backbone Model
-function book(recordData){
-  // console.log('xml:', recordData);
-  var $recordData = $(recordData);
-  var model = {
-    recordID:  textForTag(recordData, 'recordIdentifier'),
-    title:     textForTag(recordData, 'title'),
-    subtitle:  textForTag(recordData, 'subTitle'),
-    abstract:  textForTag(recordData, 'abstract'),
-    toc:       split_string(textForTag(recordData, 'tableOfContents'),'--'),
-    authors:   authors($recordData),
-    publisher: textForTag(recordData, 'publisher'),
-    isbn:      textForQuery($recordData, 'identifier[type=isbn]')
-  };
-  // console.log('model.toc', model.toc);
-  return model;
-}
 
 
-function textForTag(node, tagName) {
-  var firstTagNode = node.getElementsByTagName(tagName)[0];
-  if (firstTagNode) {
-    return firstTagNode.textContent;
-  } else {
-    return null;
-  }
-}
 
-function split_string(string, split_by){
-  if(null == string || undefined == string) {
-    return null;
-  }
-  return string.split(split_by);
-}
 
-function textForQuery(jqNode, query){
-  return _.pluck(jqNode.find(query), 'textContent');
-}
-
-// TODO: a view logic that displays only some of the authors (eg: "Gamma et al.")
-function authors($recordData){
-  var nameNodes = $recordData.find('name[type=personal]')
-  var names = _.map(nameNodes, function(node){
-    var $node = $(node);
-    var author = [
-      textForQuery($node, 'namePart[type=family]')[0],
-      textForQuery($node, 'namePart[type=given]')[0]
-    ];
-    return author;
-  });
-  // console.log('names:',names);
-  return names;
-}
 
 
 
