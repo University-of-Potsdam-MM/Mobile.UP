3dd689bdbd43b8541f6342615e428cf6cb4341fb
Richard Metzler <richard.metzler@me.com>
2014-03-20 10:21:23 +0100

    fix indentation

--- www/js/modules/nahverkehr-logic.js
+++ www/js/modules/nahverkehr-logic.js
@@ -1,10 +1,8 @@
 "use strict";
 
 var environment = 'development';
-
-
 var accessId = 'kiy4e84a4b832962eea1943106096116';
-// var endpoint = 'http://demo.hafas.de/bin/pub/vbb/extxml.exe';
+
 function endpoint(){
   if ('development' == environment){
     return '/api/transport/bin/pub/vbb/extxml.exe';
@@ -13,58 +11,56 @@ function endpoint(){
   }
 }
 
-
 // use this document for creating XML
 var doc = document.implementation.createDocument(null, null, null);
 
 // function that creates the XML structure
 function tag(nodeName, attributes) {
-    var node = doc.createElement(nodeName), text, child;
+  var node = doc.createElement(nodeName), text, child;
 
-    _.each(attributes, function(value, key){
-        node.setAttribute(key, value);
-    });
+  _.each(attributes, function(value, key){
+    node.setAttribute(key, value);
+  });
 
-    for(var i = 2; i < arguments.length; i++) {
-        child = arguments[i];
-        if(typeof child == 'string') {
-            child = doc.createTextNode(child);
-        }
-        node.appendChild(child);
+  for(var i = 2; i < arguments.length; i++) {
+    child = arguments[i];
+    if(typeof child == 'string') {
+      child = doc.createTextNode(child);
     }
+    node.appendChild(child);
+  }
 
-    return node;
+  return node;
 };
 
 function xmlString(xml) {
-    return '<?xml version="1.0" encoding="UTF-8" ?>\n' + new XMLSerializer().serializeToString(xml);
+  return '<?xml version="1.0" encoding="UTF-8" ?>\n' + new XMLSerializer().serializeToString(xml);
 }
 
 function requestExternalId(){
-    var xml =
-        tag('ReqC', {ver:'1.1', prod:'String', rt:'yes', lang:'DE', accessId:accessId},
-            tag('LocValReq', {id:'001', maxNr:20, sMode:1},
-                tag('ReqLoc', {type:'ST', match:'Lindenallee'})
-            )
-        );
-    return xmlString(xml);
+  var xml =
+    tag('ReqC', {ver:'1.1', prod:'String', rt:'yes', lang:'DE', accessId:accessId},
+      tag('LocValReq', {id:'001', maxNr:20, sMode:1},
+        tag('ReqLoc', {type:'ST', match:'Lindenallee'})
+      )
+    );
+  return xmlString(xml);
 }
 
 console.log(requestExternalId());
 
-
 // Suche abgehende Verbindungen
 function abgehendeVerbindungen(){
-    var xml =
-        tag('ReqC', {ver:'1.1', prod:'String', rt:'yes', lang:'DE', accessId:accessId},
-            tag('STBReq',{boardType:"DEP", maxJourneys:"5", sortOrder:"REALTIME"},
-                tag('Time', {}, '16:00:00'),
-                tag('Today', {}),
-                tag('TableStation', {externalId:"009230133#86"}),
-                tag('ProductFilter', {}, '1111111111111111')
-            )
-        );
-    return xmlString(xml);
+  var xml =
+    tag('ReqC', {ver:'1.1', prod:'String', rt:'yes', lang:'DE', accessId:accessId},
+      tag('STBReq',{boardType:"DEP", maxJourneys:"5", sortOrder:"REALTIME"},
+        tag('Time', {}, '16:00:00'),
+        tag('Today', {}),
+        tag('TableStation', {externalId:"009230133#86"}),
+        tag('ProductFilter', {}, '1111111111111111')
+      )
+    );
+  return xmlString(xml);
 }
 
 console.log(abgehendeVerbindungen());
