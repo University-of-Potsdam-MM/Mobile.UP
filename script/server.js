e2669e2182d4bad21fa723821ea4155608b3bf10
Richard Metzler <richard.metzler@me.com>
2014-04-21 13:46:52 +0200

    updating express, refactoring, adding moodle proxy

--- script/server.js
+++ script/server.js
@@ -2,55 +2,45 @@ var express   = require('express');
 var http      = require('http');
 var path      = require('path');
 var httpProxy = require('http-proxy');
-var reload    = require('reload');
-
-var livereload = require('express-livereload');
 
 var app = express();
 
 var config={};
 config.watchDir = process.cwd();
-livereload(app, config);
-console.log('livereload when changes occure in ' + config.watchDir);
 
-// Suche
+app.set('port', process.env.PORT || 3000);
+
+var proxy = httpProxy.createProxyServer();
+
+function proxyTo(url){
+  return function(req, res){
+    proxy.web(req, res, { target: url }, function(e) {
+      console.log(e);
+    });
+  };
+}
+
+// Bibliothekssuche
 // http://sru.gbv.de/opac-de-517?version=1.1&operation=searchRetrieve&query=java&maximumRecords=1&recordSchema=mods
-
+app.use('/api/search',    proxyTo('http://sru.gbv.de') );
 // Standort
 // http://daia.gbv.de/isil/DE-517?id=ppn:775459445&format=json
+app.use('/api/place',     proxyTo('http://daia.gbv.de') );
+app.use('/api/transport', proxyTo('http://demo.hafas.de') );
+app.use('/api/moodle',    proxyTo('https://erdmaennchen.soft.cs.uni-potsdam.de') );
 
-var bibliothekProxy = httpProxy.createServer(80, 'sru.gbv.de')
-var standortProxy   = httpProxy.createServer(80, 'daia.gbv.de');
-var nahverkehrProxy = httpProxy.createServer(80, 'demo.hafas.de')
 
-app.configure(function () {
-  app.set('port', process.env.PORT || 3000);
+app.use('/node_modules',
+  express.static(__dirname + '/node_modules'));
 
-  // app.use(express.static(__dirname + '/public'));
+app.use('/www',
+  express.static(__dirname + '/../www'));
 
-  app.use('/api/search',    bibliothekProxy);
-  app.use('/api/place',     standortProxy);
-  app.use('/api/transport', nahverkehrProxy);
-
-  app.use('/bower_components', express.static(__dirname + '/bower_components'));
-  app.use('/node_modules',     express.static(__dirname + '/node_modules'));
-  app.use('/www', express.static(__dirname + '/../www'));
+app.get("/", function(req, res){
+  res.redirect('/www/');
 });
 
-
-app.configure(function(){
-  app.use(express.errorHandler());
-});
-
-app.get("/", function(req, res){res.redirect('/www/');});
-
-var server = http.createServer(app)
-
-
-//reload code here
-reload(server, app)
-
-
+var server = http.createServer(app);
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
