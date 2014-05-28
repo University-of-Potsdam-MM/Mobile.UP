a65571f7f2e318ccc313a256ae418de013a93eb3
clickmari <maren.schulze@uni-potsdam.de>
2014-05-28 12:28:25 +0200

    Impressum

---
+++ www/js/modules/impressum.js
@@ -0,0 +1,18 @@
+define(['jquery', 'underscore', 'backbone', 'helper'], function($, _, Backbone, helper){
+
+	var ImpressumPageView = Backbone.View.extend({
+		attributes: {"id": "impressum"},
+
+		initialize: function(){
+			this.template = helper.rendertmpl('impressum');
+		},
+
+		render: function(){
+			$(this.el).html(this.template({}));
+			$(this.el).trigger("create");
+			return this;
+		}
+	});
+
+	return ImpressumPageView;
+});
