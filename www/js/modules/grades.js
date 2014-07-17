76ff98a288bb48e24ac259e613ab365b82e4a4b7
Hendrik Geßner <HENGE01@gmail.com>
2014-07-17 15:38:03 +0200

    Adresses #73 - Implemented grades. Password input issues still pending.

--- www/js/modules/grades.js
+++ www/js/modules/grades.js
@@ -1,15 +1,73 @@
 define(['jquery', 'underscore', 'backbone', 'utils'], function($, _, Backbone, utils){
 
+	var Grades = Backbone.Collection.extend({
+		
+		parse: function(result) {
+			return result.achievements.acm;
+		}
+	});
+	
+	var GradesView = Backbone.View.extend({
+		
+		initialize: function() {
+			this.template = utils.rendertmpl("gradeList");
+			this.listenTo(this.collection, "sync", this.render);
+		},
+		
+		render: function() {
+			this.$el.empty();
+			this.$el.append(this.template({grades: this.collection.toJSON()}));
+			this.$el.trigger("create");
+		}
+	});
+	
+	var PasswordInputView = Backbone.View.extend({
+		
+		events: {
+			"click #login": "login"
+		},
+		
+		initialize: function() {
+			this.listenTo(this.collection, "sync", function(model) {
+				console.log("Loginvorgang abgeschlossen");
+			});
+		},
+		
+		login: function(ev) {
+			ev.preventDefault();
+			
+			console.log("Loginvorgang gestartet");
+			
+			var user = $("input[name='benutzer']").val();
+			var pw = $("input[name='password']").val();
+			
+			this.collection.url = "https://fossa.soft.cs.uni-potsdam.de:8243/services/pulsAPI?action=acm&auth=H2LHXK5N9RDBXMB";
+			this.collection.url += "&user=" + encodeURIComponent(user);
+			this.collection.url += "&password=" + encodeURIComponent(pw);
+			
+			this.collection.fetch();
+		}
+	});
+	
 	var GradesPageView = Backbone.View.extend({
 		attributes: {"id": "grades"},
 
 		initialize: function(){
 			this.template = utils.rendertmpl('grades');
+			this.listenToOnce(this, "render", this.prepareGrade);
+			
+			this.grades = new Grades;
 		},
-
+		
+		prepareGrade: function() {
+			new PasswordInputView({collection: this.grades, el: this.$("#loginForm")});
+			new GradesView({collection: this.grades, el: this.$("#gradesTable")});
+		},
+		
 		render: function(){
 			$(this.el).html(this.template({}));
 			$(this.el).trigger("create");
+			this.trigger("render");
 			return this;
 		}
 	});
