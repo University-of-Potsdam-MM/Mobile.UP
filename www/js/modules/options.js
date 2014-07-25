f292028772ddabef211727cbd1a9567404b34ed1
Alexander Kiy <alekiy@uni-potsdam.de>
2014-07-25 18:46:37 +0200

    options handles login and logout requests

--- www/js/modules/options.js
+++ www/js/modules/options.js
@@ -10,23 +10,47 @@ define([
 
 	var OptionsPageView = Backbone.View.extend({
 
+		model: Session,
 		attributes: {"id": 'options'},
-		template: utils.rendertmpl('options'),
+
+		logintemplate: utils.rendertmpl('login'),
+		logouttemplate: utils.rendertmpl('logout'),
 
 		events: {
-			'submit form': 'submit'
+			'submit #loginform': 'login',
+			'submit #logoutform': 'logout'
 		},
 
 		render: function(){
-			$(this.el).html(this.template({}));
+
+			if (this.model.get('authenticated')){
+				$(this.el).html(this.logouttemplate({}));
+			}else{
+				$(this.el).html(this.logintemplate({}));
+			}
+
+			$(this.el).trigger("create");
 			return this;
 		},
 
-		submit: function(ev){
+		login: function(ev){
 			ev.preventDefault();
 			var username = $('#username').val();
 			var password = $('#password').val();
-			Session.login({username: username, password: password});
+			this.model.login({username: username, password: password});
+			if(this.get('redirectFrom')){
+				var path = this.get('redirectFrom');
+				this.unset('redirectFrom');
+				Backbone.history.navigate(path, { trigger : true });
+			}else{
+				Backbone.history.navigate('', { trigger : true });
+			}
+		},
+
+		logout: function(ev){
+			ev.preventDefault();
+			this.model.logout();
+			this.render();
 		}
 
 	});
