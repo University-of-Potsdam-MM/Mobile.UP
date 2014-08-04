University-of-Potsdam-MM/Mobile.UP

commit 0d53c6bb1d3c1bc61c78006e10f766fc890c4e89

Closes #129 - Found memory and event leak and fixed it.

www/js/router.js
@@ -151,6 +151,10 @@ define([
 		},
 
 		changePage: function(page){
+			if (this.currentView) {
+				// Release memory, stop listening to events
+				this.currentView.remove();
+			}
 
 			// prepare new view for DOM display
 			$(page.el).attr('data-role', 'page');

commit 7e2076f1f8de65950cd057b4367740ab36ed69b5

Closes #154 - Fixed scrolling bug that was introduced in 0d53c6bb1d3c1bc61c78006e10f766fc890c4e89.

www/js/router.js
@@ -210,8 +210,8 @@ define([
 
 		changePage: function(page){
 			if (this.currentView) {
-				// Release memory, stop listening to events
-				this.currentView.remove();
+				// Stop listening to events
+				this.currentView.stopListening();
 			}
 
 			// prepare new view for DOM display