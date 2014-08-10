05a9fc10b38057f6babc26b2b3e730e26272ce1e
Hendrik Geßner <HENGE01@gmail.com>
2014-08-10 13:10:06 +0200

    Adresses #162 - Removed unnecessary icon copy code.

--- script/copy-icons.sh
+++ script/copy-icons.sh
@@ -1,10 +1,3 @@
-# App Icons
-cp www/res/icon/android/icon-96-xhdpi.png platforms/android/res/drawable/icon.png
-cp www/res/icon/android/icon-72-hdpi.png platforms/android/res/drawable-hdpi/icon.png
-cp www/res/icon/android/icon-36-ldpi.png platforms/android/res/drawable-ldpi/icon.png
-cp www/res/icon/android/icon-48-mdpi.png platforms/android/res/drawable-mdpi/icon.png
-cp www/res/icon/android/icon-96-xhdpi.png platforms/android/res/drawable-xhdpi/icon.png
-
 # Splashscreen
 cp www/res/screen/android/screen-ldpi-portrait.png platforms/android/res/drawable-port-ldpi/screen.png
 cp www/res/screen/android/screen-ldpi-landscape.png platforms/android/res/drawable-land-ldpi/screen.png
