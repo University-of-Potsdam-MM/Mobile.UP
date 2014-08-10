afdc3a263badd8a5db6fb65ac63bd265991b7492
Hendrik Geßner <HENGE01@gmail.com>
2014-08-10 16:52:05 +0200

    Adresses #166 - Switched to git version of the inappbrowser plugin.

--- script/add-plugins.sh
+++ script/add-plugins.sh
@@ -1,2 +1,4 @@
-phonegap plugin add org.apache.cordova.inappbrowser
+phonegap plugin remove org.apache.cordova.inappbrowser
+phonegap plugin add https://github.com/apache/cordova-plugin-inappbrowser
+
 phonegap plugin add org.apache.cordova.splashscreen
