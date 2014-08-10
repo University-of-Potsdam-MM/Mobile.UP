506ab0733870b3423f3ed8df8301ab6b7028fcc1
Hendrik Geßner <HENGE01@gmail.com>
2014-08-10 17:07:13 +0200

    Adresses #165 - Switched to git version of the splashscreen plugin, increased splashscreen timeout to ten seconds.

--- script/add-plugins.sh
+++ script/add-plugins.sh
@@ -1,4 +1,5 @@
 phonegap plugin remove org.apache.cordova.inappbrowser
 phonegap plugin add https://github.com/apache/cordova-plugin-inappbrowser
 
-phonegap plugin add org.apache.cordova.splashscreen
+phonegap plugin remove org.apache.cordova.splashscreen
+phonegap plugin add https://github.com/apache/cordova-plugin-splashscreen
