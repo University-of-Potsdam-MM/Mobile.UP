4b3641b3790f7c6614912f06f3872798f4b4d577
Hendrik Geßner <HENGE01@gmail.com>
2014-09-05 15:10:26 +0200

    Adresses #205 - Added necessary plugins.

--- script/add-plugins.sh
+++ script/add-plugins.sh
@@ -3,3 +3,12 @@ phonegap plugin add https://github.com/apache/cordova-plugin-inappbrowser
 
 phonegap plugin remove org.apache.cordova.splashscreen
 phonegap plugin add https://github.com/apache/cordova-plugin-splashscreen
+
+phonegap plugin remove org.apache.cordova.dialogs
+phonegap plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-dialogs.git
+
+phonegap plugin remove nl.x-services.plugins.calendar
+phonegap plugin add https://github.com/EddyVerbruggen/Calendar-PhoneGap-Plugin.git
+
+phonegap plugin remove org.apache.cordova.device
+phonegap plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device.git
