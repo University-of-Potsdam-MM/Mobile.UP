61b5c3e4cc5f84849313fa86691d31b5a3d90a4c
Hendrik Geßner <HENGE01@gmail.com>
2014-11-21 20:32:03 +0100

    Closes #362 - Make username all lower case on login.

--- www/js/modules/options.js
+++ www/js/modules/options.js
@@ -49,11 +49,15 @@ define([
 				var username = $('#username').val();
 				var password = $('#password').val();
 				
+				// Remove mail suffix, only username is needed
 				suffixIndex = username.indexOf("@");
 				if (suffixIndex != -1) {
 					username = username.substr(0, suffixIndex);
 					$('#username').val(username);
 				}
+				
+				// Usernames have to be all lower case, otherwise some service logins will fail
+				$('#username').val(username.toLowerCase());
 				
 				this.model.generateLoginURL({username: username, password: password});
 				if (!this.LoadingView) {this.LoadingView = new utils.LoadingView({model: this.model, el: this.$("#loadingSpinner")});}
