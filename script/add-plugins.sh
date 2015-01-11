78d1372bcd45ca565e1280bc10dde84139a88ca0
Hendrik Geßner <HENGE01@gmail.com>
2015-01-11 19:01:52 +0100

    Adresses #337 - Fixed missing plugin error.

--- script/add-plugins.sh
+++ script/add-plugins.sh
@@ -19,5 +19,8 @@ phonegap plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-netwo
 phonegap plugin remove com.borismus.webintent
 phonegap plugin add https://github.com/Initsogar/cordova-webintent.git
 
+phonegap plugin remove org.apache.cordova.file
+phonegap plugin add https://github.com/apache/cordova-plugin-file.git
+
 phonegap plugin remove org.apache.cordova.file-transfer
 phonegap plugin add https://github.com/apache/cordova-plugin-file-transfer.git
