69aec949345ffc102b14deb2ec90025373894cf2
Hendrik Geßner <HENGE01@gmail.com>
2015-01-15 23:36:17 +0100

    Updated plugin references, removed plugin removal code.

--- script/add-plugins.sh
+++ script/add-plugins.sh
@@ -1,29 +1,10 @@
-phonegap plugin remove org.apache.cordova.inappbrowser
 phonegap plugin add https://github.com/apache/cordova-plugin-inappbrowser
-
-phonegap plugin remove org.apache.cordova.splashscreen
 phonegap plugin add https://github.com/apache/cordova-plugin-splashscreen
-
-phonegap plugin remove org.apache.cordova.dialogs
-phonegap plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-dialogs.git
-
-phonegap plugin remove nl.x-services.plugins.calendar
+phonegap plugin add https://github.com/apache/cordova-plugin-dialogs.git
 phonegap plugin add https://github.com/EddyVerbruggen/Calendar-PhoneGap-Plugin.git
-
-phonegap plugin remove org.apache.cordova.device
-phonegap plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device.git
-
-phonegap plugin remove org.apache.cordova.network-information
-phonegap plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-network-information.git
-
-phonegap plugin remove com.borismus.webintent
+phonegap plugin add https://github.com/apache/cordova-plugin-device.git
+phonegap plugin add https://github.com/apache/cordova-plugin-network-information.git
 phonegap plugin add https://github.com/Initsogar/cordova-webintent.git
-
-phonegap plugin remove org.apache.cordova.file
 phonegap plugin add https://github.com/apache/cordova-plugin-file.git
-
-phonegap plugin remove org.apache.cordova.file-transfer
 phonegap plugin add https://github.com/apache/cordova-plugin-file-transfer.git
-
-phonegap plugin remove nl.x-services.plugins.toast
 phonegap plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
