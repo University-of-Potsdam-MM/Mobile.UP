63bbe14c5837907f06dcbaafbc84ec3121ec695b
Hendrik Geßner <HENGE01@gmail.com>
2015-05-30 18:36:03 +0200

    Adresses #423 - Enabled course export for single dates.

--- www/js/modules/calendar.common.js
+++ www/js/modules/calendar.common.js
@@ -151,7 +151,13 @@ define([
 		},
 
 		exportToCalendar: function(entry, course, callback) {
-			//callback(entry);
+			var split = this.get("timespan").split(' ');
+			var dayContent = moment(split[1], "DD.MM.YYYY");
+			
+			entry.startDate = this.getBegin(dayContent).toDate();
+			entry.endDate = this.getEnd(dayContent).toDate();
+
+			callback(entry);
 		}
 	});
 
@@ -164,7 +170,6 @@ define([
 		exportToCalendar: function(entry, course, callback) {
 			entry.startDate = this.getBegin(course.getStarting()).toDate();
 			entry.endDate = this.getEnd(course.getStarting()).toDate();
-
 			entry.options.recurrence = "weekly";
 			//add one day to make sure last event is also synced!
 			entry.options.recurrenceEndDate = moment(course.getEnding()).add(1, "days").toDate();
