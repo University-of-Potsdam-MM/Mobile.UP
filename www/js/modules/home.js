001e5af0a802a661fb424308fe53a21cfadbbd6a
mantir <mkapp2@gmx.de>
2015-06-19 16:06:05 +0200

    home template

--- www/js/modules/home.js
+++ www/js/modules/home.js
@@ -1,18 +1,53 @@
 define(['jquery', 'underscore', 'backbone', 'utils'], function($, _, Backbone, utils){
-
-	var HomePageView = Backbone.View.extend({
-		attributes: {"id": 'home'},
-
+	
+	app.views.MainPage = Backbone.View.extend({
 		initialize: function(){
-			this.template = utils.rendertmpl('home');
+			_.bindAll(this, 'render');
 		},
 
 		render: function(){
-			$(this.el).html(this.template({}));
-			$(this.el).trigger("create");
+			$(this.el).html('');
 			return this;
 		}
 	});
+	
+	app.views.MainMenu = Backbone.View.extend({
+		attributes: {"id": 'home'},
 
-	return HomePageView;
+		initialize: function(p){
+			this.page = p.page;
+			this.template = utils.rendertmpl('home');
+			_.bindAll(this, 'render');
+		},
+
+		render: function(){
+			this.$el = this.page;
+			this.$el.attr('id', 'home');
+			this.$el.html(this.template({}));
+			this.$el.trigger("create");
+			return this;
+		}
+	});
+	
+	
+	app.views.MainLogout = Backbone.View.extend({
+
+		initialize: function(p){
+			this.template = utils.rendertmpl('logout');
+			this.page = p.page;
+			_.bindAll(this, 'render');
+		},
+
+		render: function(){
+			this.$el = this.page;
+			this.$el.attr('id', 'logout');
+			this.$el.html(this.template({}));
+			$.mobile.changePage.defaults.reverse = false;
+			window.setTimeout(function(){$('.settings').hide();}, 100) //Logoutbutton oben rechts verstecken
+			$('.back').attr('href', '#home');
+			this.$el.trigger("create");
+			return this;
+		},
+	});
+	
 });
