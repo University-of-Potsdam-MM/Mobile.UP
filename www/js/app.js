57518e0f832b0029480fdaf93852e2b25766a416
Hendrik Geßner <HENGE01@gmail.com>
2015-10-09 14:35:23 +0200

    Moved content loading related tasks into contentLoader object.

--- www/js/app.js
+++ www/js/app.js
@@ -1,122 +1,4 @@
 var app = {models:{},views:{},controllers:{}};
-var initContentAndData = function (c) {
-	var content = false;
-	if (!app.data[c])
-		app.data[c] = {};
-	return content;
-};
-var setFetchedContent = function (content, s, d, params, response, c, a, _) {
-	console.log('content');
-	if (content.fetchSuccess)
-		content.fetchSuccess(s, d);
-	content.p = params;
-	if (content.beforeRender)
-		content.beforeRender();
-	if (s == 'set') { //Model aus collection geholt
-		if (content.model) {
-			content.model.set(d);
-			content.model.p = params;
-		}
-	} else if (s == 'cached') { //Daten aus dem Cache geholt
-		if (content.model) {
-			content.model.set(content.model.parse(d));
-			content.model.p = params;
-		}
-		if (content.collection) {
-			content.collection.set(content.collection.parse(d));
-			content.collection.p = params;
-		}
-	} else { //Daten vom Server geholt
-		if (content.collection) {
-			response = d = content.collection.toJSON();
-			content.collection.p = params;
-			if (content.collection.response)
-				response = content.collection.response;
-		}
-
-		if (content.model && content.model.toJSON) {
-			response = d = content.model.toJSON();
-			content.model.p = params;
-			if (content.model.response)
-				response = content.model.response;
-		}
-	}
-	if (_.keys(response).length > 0) {
-		if (!app.data[c])
-			app.data[c] = {};
-		app.data[c][a] = response; //Daten speichern
-		if (content.model)
-			app.appCache.setCache(content.model.url, response);
-		else if (content.collection) {
-			app.appCache.setCache(content.collection.url, response);
-		}
-	}
-	return {d: d, response: response};
-};
-var resolveWithContent = function (response, q, content, d, _) {
-	if (_.keys(response).length > 0)
-		q.resolve(response, content);
-	else
-		q.resolve(d, content);
-};
-var retreiveElementFromLoadedCollection = function (content, params, d, _) {
-	if ((content.model || content.collection) && content.inCollection) { //Element aus der geladenen Collection holen und nicht vom Server
-		var parts = content.inCollection.split('.');
-		try {
-			var list = eval('app.data.' + content.inCollection);
-		} catch (e) {
-		}
-		if (list) {
-			try {
-				var filteredList = _.filter(list, function (item) {
-					return _.some(item, function (item) {
-						return eval('item.' + content.idInCollection) == params.id;
-					});
-				});
-			} catch (e) {
-			}
-		}
-		if (filteredList) //Element in Liste gefunden
-			d = filteredList[0];
-	}
-	return d;
-};
-var retreiveOrFetchContent = function (content, success, d, $, utils, _) {
-	if (content.collection) { //Content hat eine Collection
-		if (app.appCache.getCache(content.collection.url)) {
-			success('cached', app.appCache.getCache(content.collection.url));
-		} else if (content.collection.url && (!content.model || typeof content.model.url != 'function')) { //Collection abrufbar von URL
-			content.collection.fetch({
-				success: success,
-				error: function () {
-				},
-				dataType: 'json'
-			});
-		} else {
-			success();
-		}
-	} else if (content.model) { //Content hat ein Model
-		console.log('Model');
-		if (_.keys(d).length > 0) { //Model bereits in Collection gefunden
-			success('set', d);
-		}
-		else if (app.appCache.getCache(content.model.url)) { //Model in cache
-			success('cached', app.appCache.getCache(content.model.url));
-		} else if (content.model.url && typeof content.model.url != 'function') { //Model abrufbar von URL
-			console.log(content.model);
-			content.model.fetch($.extend(utils.cacheDefaults(), {
-				success: success,
-				error: function () {
-				},
-				dataType: 'json'
-			}));
-		} else {
-			success();
-		}
-	} else { //Content einfach so
-		success();
-	}
-};
 define([
 	'jquery',
 	'underscore',
@@ -349,7 +231,7 @@ define([
 				var reverse = __ret.reverse;
 				var transition = __ret.transition;
 
-				var content = initContentAndData(c);
+				var content = contentLoader.initContentAndData(c);
 				/**
 				 * Success function, die nachdem Daten vom Server oder aus dem Cache geholt wurden, oder wenn nichts zu holen ist, ausgeführt wird.
 				 * @param s state object (After request with model.fetch or collection.fetch or custom: 'cached' or 'set' to indicate whether it was fetched from cache or from a collection)
@@ -357,12 +239,12 @@ define([
 				 */
 				var success = function (s, d) {
 					if (content) {
-						var __ret = setFetchedContent(content, s, d, params, response, c, a, _);
+						var __ret = contentLoader.setFetchedContent(content, s, d, params, response, c, a, _);
 						d = __ret.d;
 						response = __ret.response;
 						viewContainer.finishRendering(content, pageTitle, pageContent, $pageContainer, utils, $);
 					}
-					resolveWithContent(response, q, content, d, _);
+					contentLoader.resolveWithContent(response, q, content, d, _);
 				};
 
 				/**
@@ -371,8 +253,8 @@ define([
 				var afterTransition = function () {
 					if (app.views[utils.capitalize(c) + utils.capitalize(a)]) { //Wenn eine View-Klasse für Content vorhanden ist: ausführen
 						content = viewContainer.setCurrentView(params, page, content, c, a, app, utils);
-						d = retreiveElementFromLoadedCollection(content, params, d, _);
-						retreiveOrFetchContent(content, success, d, $, utils, _);
+						d = contentLoader.retreiveElementFromLoadedCollection(content, params, d, _);
+						contentLoader.retreiveOrFetchContent(content, success, d, $, utils, _);
 					} else { //Wenn keine Viewklasse vorhanden ist, die page als view nehmen
 						viewContainer.usePageAsView(page, app);
 						success();
--- www/js/contentLoader.js
+++ www/js/contentLoader.js
@@ -10,7 +10,128 @@ define([
 ], function($, _, Backbone, BackboneMVC, _str, utils, Q, customHistory) {
 
     var ContentLoader = {
+        initContentAndData: function (c) {
+            var content = false;
+            if (!app.data[c])
+                app.data[c] = {};
+            return content;
+        },
 
+        setFetchedContent: function (content, s, d, params, response, c, a, _) {
+            console.log('content');
+            if (content.fetchSuccess)
+                content.fetchSuccess(s, d);
+            content.p = params;
+            if (content.beforeRender)
+                content.beforeRender();
+            if (s == 'set') { //Model aus collection geholt
+                if (content.model) {
+                    content.model.set(d);
+                    content.model.p = params;
+                }
+            } else if (s == 'cached') { //Daten aus dem Cache geholt
+                if (content.model) {
+                    content.model.set(content.model.parse(d));
+                    content.model.p = params;
+                }
+                if (content.collection) {
+                    content.collection.set(content.collection.parse(d));
+                    content.collection.p = params;
+                }
+            } else { //Daten vom Server geholt
+                if (content.collection) {
+                    response = d = content.collection.toJSON();
+                    content.collection.p = params;
+                    if (content.collection.response)
+                        response = content.collection.response;
+                }
+
+                if (content.model && content.model.toJSON) {
+                    response = d = content.model.toJSON();
+                    content.model.p = params;
+                    if (content.model.response)
+                        response = content.model.response;
+                }
+            }
+            if (_.keys(response).length > 0) {
+                if (!app.data[c])
+                    app.data[c] = {};
+                app.data[c][a] = response; //Daten speichern
+                if (content.model)
+                    app.appCache.setCache(content.model.url, response);
+                else if (content.collection) {
+                    app.appCache.setCache(content.collection.url, response);
+                }
+            }
+            return {d: d, response: response};
+        },
+
+        resolveWithContent: function (response, q, content, d, _) {
+            if (_.keys(response).length > 0)
+                q.resolve(response, content);
+            else
+                q.resolve(d, content);
+        },
+
+        retreiveElementFromLoadedCollection: function (content, params, d, _) {
+            if ((content.model || content.collection) && content.inCollection) { //Element aus der geladenen Collection holen und nicht vom Server
+                var parts = content.inCollection.split('.');
+                try {
+                    var list = eval('app.data.' + content.inCollection);
+                } catch (e) {
+                }
+                if (list) {
+                    try {
+                        var filteredList = _.filter(list, function (item) {
+                            return _.some(item, function (item) {
+                                return eval('item.' + content.idInCollection) == params.id;
+                            });
+                        });
+                    } catch (e) {
+                    }
+                }
+                if (filteredList) //Element in Liste gefunden
+                    d = filteredList[0];
+            }
+            return d;
+        },
+
+        retreiveOrFetchContent: function (content, success, d, $, utils, _) {
+            if (content.collection) { //Content hat eine Collection
+                if (app.appCache.getCache(content.collection.url)) {
+                    success('cached', app.appCache.getCache(content.collection.url));
+                } else if (content.collection.url && (!content.model || typeof content.model.url != 'function')) { //Collection abrufbar von URL
+                    content.collection.fetch({
+                        success: success,
+                        error: function () {
+                        },
+                        dataType: 'json'
+                    });
+                } else {
+                    success();
+                }
+            } else if (content.model) { //Content hat ein Model
+                console.log('Model');
+                if (_.keys(d).length > 0) { //Model bereits in Collection gefunden
+                    success('set', d);
+                }
+                else if (app.appCache.getCache(content.model.url)) { //Model in cache
+                    success('cached', app.appCache.getCache(content.model.url));
+                } else if (content.model.url && typeof content.model.url != 'function') { //Model abrufbar von URL
+                    console.log(content.model);
+                    content.model.fetch($.extend(utils.cacheDefaults(), {
+                        success: success,
+                        error: function () {
+                        },
+                        dataType: 'json'
+                    }));
+                } else {
+                    success();
+                }
+            } else { //Content einfach so
+                success();
+            }
+        }
     };
 
     return ContentLoader;
