a8678df06aa48d51b9a02b92d8a04ff127f98984
Hendrik Geßner <HENGE01@gmail.com>
2015-11-27 16:40:51 +0100

    Added AMD wrappers to controllers.

--- www/js/controllers/campus.js
+++ www/js/controllers/campus.js
@@ -1,69 +1,72 @@
-/*
-* CampusController
-*/
-app.controllers.campus = BackboneMVC.Controller.extend({
-    name: 'campus',
-	modules: {
-		'sitemap' : 'sitemapView',
-		'room' : 'roomView',
-		'opening' : 'openingView',
-		'transport' : 'transportView',
-		'transport2' : 'transportPlanView', 
-		'mensa' : 'mensaView',
-		'emergency' : 'emergencyView',
-		'library' : 'libraryView',
-		'people' : 'peopleView',
-		'impressum' : 'impressumView',
-	},
-	/*
-	* Um Initialisierungsfunktionen auszuführen
-	*/
-    init:function(){
-    },
-	
-	default:function(){
-		this.index();
-	},
-	
-	sitemap:function(){
-		app.loadPage('sitemap', 'index');
-    },
+define(["backboneMVC", "backbone"], function(BackboneMVC, Backbone) {
 
-    room:function(){
-		app.loadPage('room', 'index');
-    },
-	
-	opening:function(){
-		app.loadPage('opening', 'index');
-    },
-	
-	transport:function(type, campus){
-		if(type == 'plan')
-			app.loadPage('transport2', 'index');
-    	else
-			app.loadPage('transport', 'index');
-    },
-	
-	mensa:function(){
-		app.loadPage('mensa', 'index');
-    },
-	
-	emergency:function(){
-		app.loadPage('emergency', 'index');
-    },
-	
-	library:function(detail){
-		console.log(Backbone.history.fragment.replace('detail', ''));
-		if(detail == 'detail')
-			app.route(Backbone.history.fragment.replace('detail', ''), true, true);
-		app.loadPage('library', 'index');
-    },
-	
-	people:function(){
-		app.loadPage('people', 'index');
-    },
-	
-	impressum:function(){
-		app.loadPage('impressum', 'index');
-    },
+	/*
+	 * CampusController
+	 */
+	app.controllers.campus = BackboneMVC.Controller.extend({
+		name: 'campus',
+		modules: {
+			'sitemap' : 'sitemapView',
+			'room' : 'roomView',
+			'opening' : 'openingView',
+			'transport' : 'transportView',
+			'transport2' : 'transportPlanView',
+			'mensa' : 'mensaView',
+			'emergency' : 'emergencyView',
+			'library' : 'libraryView',
+			'people' : 'peopleView',
+			'impressum' : 'impressumView',
+		},
+		/*
+		 * Um Initialisierungsfunktionen auszuführen
+		 */
+		init:function(){
+		},
+
+		default:function(){
+			this.index();
+		},
+
+		sitemap:function(){
+			app.loadPage('sitemap', 'index');
+		},
+
+		room:function(){
+			app.loadPage('room', 'index');
+		},
+
+		opening:function(){
+			app.loadPage('opening', 'index');
+		},
+
+		transport:function(type, campus){
+			if(type == 'plan')
+				app.loadPage('transport2', 'index');
+			else
+				app.loadPage('transport', 'index');
+		},
+
+		mensa:function(){
+			app.loadPage('mensa', 'index');
+		},
+
+		emergency:function(){
+			app.loadPage('emergency', 'index');
+		},
+
+		library:function(detail){
+			console.log(Backbone.history.fragment.replace('detail', ''));
+			if(detail == 'detail')
+				app.route(Backbone.history.fragment.replace('detail', ''), true, true);
+			app.loadPage('library', 'index');
+		},
+
+		people:function(){
+			app.loadPage('people', 'index');
+		},
+
+		impressum:function(){
+			app.loadPage('impressum', 'index');
+		},
+	});
 });
--- www/js/controllers/studies.js
+++ www/js/controllers/studies.js
@@ -1,58 +1,61 @@
-/*
-* studiesController
-*/
-app.controllers.studies = BackboneMVC.Controller.extend({
-    name: 'studies',
-	modules: {
-		'calendar' : 'calendarView',
-		'calendar.export' : 'calendarExportView',
-		'lectures' : 'lecturesView',
-		'grades' : 'gradesView',
-		'moodle' : 'moodleView'
-	},
-	/*
-	* Um Initialisierungsfunktionen auszuführen
-	*/
-    init:function(){
-    },
-	
-	default:function(){
-		
-	},
-	
-	calendar:function(day){
-		if(day == 'export') {
-			app.loadPage('calendarExport', 'index', {day:day});
-		} else {
-			app.loadPage('calendar', 'index', {day:day});
-		}
-    },
+define(["backboneMVC"], function(BackboneMVC) {
 
-    lectures:function(vvzUrls){
-		var _this = this;
-		app.loadPage('lectures', 'index').done(function(){
-			var vvzHistory = app.currentView.vvzHistory;
-			console.log(vvzUrls);
-			if (vvzUrls != undefined) {
-				vvzHistory.reset(JSON.parse(vvzUrls));
+	/*
+	 * studiesController
+	 */
+	app.controllers.studies = BackboneMVC.Controller.extend({
+		name: 'studies',
+		modules: {
+			'calendar': 'calendarView',
+			'calendar.export': 'calendarExportView',
+			'lectures': 'lecturesView',
+			'grades': 'gradesView',
+			'moodle': 'moodleView'
+		},
+		/*
+		 * Um Initialisierungsfunktionen auszuführen
+		 */
+		init: function () {
+		},
+
+		default: function () {
+
+		},
+
+		calendar: function (day) {
+			if (day == 'export') {
+				app.loadPage('calendarExport', 'index', {day: day});
 			} else {
-				vvzHistory.reset();
+				app.loadPage('calendar', 'index', {day: day});
 			}
-	
-			/*_this.listenTo(app.currentView, "openVvzUrl", function(vvzHistory) {
-				var param = JSON.stringify(vvzHistory.toJSON());
-				var url = "studies/lectures/" + encodeURIComponent(param)
-			});*/
-		});
-    },	
-	
-	grades:function(){
-		app.loadPage('grades', 'index');
-    },
-	
-	moodle:function(action, id){ //action ist immer index, bis jemand das ändern möchte
-		action = 'index';
-		app.loadPage('moodle', action, {model: app.session, courseid: id});
-    },
-	
+		},
+
+		lectures: function (vvzUrls) {
+			var _this = this;
+			app.loadPage('lectures', 'index').done(function () {
+				var vvzHistory = app.currentView.vvzHistory;
+				console.log(vvzUrls);
+				if (vvzUrls != undefined) {
+					vvzHistory.reset(JSON.parse(vvzUrls));
+				} else {
+					vvzHistory.reset();
+				}
+
+				/*_this.listenTo(app.currentView, "openVvzUrl", function(vvzHistory) {
+				 var param = JSON.stringify(vvzHistory.toJSON());
+				 var url = "studies/lectures/" + encodeURIComponent(param)
+				 });*/
+			});
+		},
+
+		grades: function () {
+			app.loadPage('grades', 'index');
+		},
+
+		moodle: function (action, id) { //action ist immer index, bis jemand das ändern möchte
+			action = 'index';
+			app.loadPage('moodle', action, {model: app.session, courseid: id});
+		},
+
+	});
 });
