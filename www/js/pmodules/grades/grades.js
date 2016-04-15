e916df2192c77c467e6a70075ab6abc548272c4c
Hendrik Geßner <HENGE01@gmail.com>
2016-04-15 15:18:42 +0200

    Switched to grades 2.0 prototype.

--- www/js/pmodules/grades/grades.js
+++ www/js/pmodules/grades/grades.js
@@ -9,10 +9,38 @@ define(['jquery', 'underscore', 'backbone', 'utils', 'Session'], function($, _, 
 			this.url = "https://api.uni-potsdam.de/endpoints/pulsAPI/1.0?action=acm&auth=H2LHXK5N9RDBXMB&datatype=json2";
 			this.url += "&user=" + encodeURIComponent(this.session.get('up.session.username'));
 			this.url += "&password=" + encodeURIComponent(this.session.get('up.session.password'));
+
+			this.url = "http://localhost/upapp/grades-spike/data.json";
 		},
 
-		parse: function(result) {
-			return result.jsonObject;
+		parse: function(data) {
+			var achievements = data.academicAchievements.achievement;
+			achievements.field = _.map(achievements.field, this.parseModule, this);
+
+			return {
+				achievements: achievements
+			};
+		},
+
+		parseModule: function(module) {
+			module.id = _.uniqueId("field");
+
+			if (module.module)
+				module.module = this.asArray(module.module);
+			module.module = _.map(module.module, this.parseModule, this);
+
+			module.examination = module.examination || {};
+			module.examination.graded = module.examination.graded || [];
+
+			return module;
+		},
+
+		asArray: function(subject) {
+			if (Array.isArray(subject)) {
+				return subject;
+			} else {
+				return [subject];
+			}
 		}
 	});
 
@@ -25,7 +53,7 @@ define(['jquery', 'underscore', 'backbone', 'utils', 'Session'], function($, _, 
 
 		render: function() {
 			this.$el.empty();
-			this.$el.append(this.template({grades: this.model.get("grades")}));
+			this.$el.append(this.template({data: this.model.toJSON()}));
 			this.$el.trigger("create");
 		}
 	});
