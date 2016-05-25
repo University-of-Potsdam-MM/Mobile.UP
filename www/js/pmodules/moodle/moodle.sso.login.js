38c33f2b79cde7473cdbd183a3f25b14ce85cca5
Hendrik Geßner <HENGE01@gmail.com>
2016-05-25 16:13:30 +0200

    Added documentation and error handling.

--- www/js/pmodules/moodle/moodle.sso.login.js
+++ www/js/pmodules/moodle/moodle.sso.login.js
@@ -10,13 +10,18 @@ define([
     'uri/URI'
 ], function( $, _, Backbone, utils, Session, URI) {
 
+    var ERROR_TECHNICAL = 1;
+    var ERROR_CREDENTIALS = 2;
+
     /*
     Moodle token retrieval and Moodle SSO login, general process
     1. Call the Moodle plugin
       1.1 If a login is required the browser gets redirected to the Moodle login page
       1.2 If the user is already logged in, a token is retrieved and the process ends
     2. On the Moodle login page the SSO / IdP login link must be followed
-    3. Once in the login mask the credentials have to be inserted and the login form must be submitted. If the login succeeds we are taken back to the Moodle plugin
+    3. Once in the login mask the credentials have to be inserted and the login form must be submitted.
+      3.1 If the login succeeds we are taken back to the Moodle plugin
+      3.2 If the login fails, we are taken back to the login mask
      */
     var actions = {
 
@@ -41,11 +46,16 @@ define([
 
                     token = token.split(":::")[1];
                     console.log("Moodle token found: " + token);
+
+                    var session = loginRequest.session;
+                    session.set('up.session.MoodleToken', token);
+                    session.set('up.session.authenticated', true);
+
+                    loginRequest.success();
                 } catch (err) {
                     // error happened
+                    loginRequest.error(ERROR_TECHNICAL);
                 }
-
-                browser.close();
             }
         },
 
@@ -53,24 +63,22 @@ define([
             type: "loadstop",
             predicate: function (ev) { return ev.url === loginUrl },
             action: function (ev, loginRequest, browser) {
-                console.log("Login required");
+                console.log("IdP link required");
                 var startLogin = 'var links = document.getElementsByTagName("a");' +
                                  'for (var i = 0; i < links.length; i++) {' +
                                  '  if (links[i].innerHTML.indexOf("Login via") != -1)' +
                                  '    window.open(links[i].href);' +
                                  '}';
 
-                browser.executeScript({code: startLogin}, function (result) {
-                    console.log(JSON.stringify(result));
-                });
+                browser.executeScript({code: startLogin}, function (result) {});
             }
         },
 
         ssoLogin: {
             type: "loadstop",
-            predicate: function (ev) { return ev.url === idpUrl; },
+            predicate: function (ev, loginRequest) { return ev.url === idpUrl && !loginRequest.loginAttemptStarted; },
             action: function (ev, loginRequest, browser) {
-                console.log("Login required");
+                console.log("Login inject required");
 
                 var session = loginRequest.session;
                 var user = session.get("up.session.username");
@@ -81,9 +89,31 @@ define([
                                        'pw[0].value = ' + JSON.stringify(pw) + ';' +
                                        'document.forms["login"].submit()';
 
-                browser.executeScript({ code: enterCredentials }, function(result) {
-                    console.log(JSON.stringify(result));
-                });
+                loginRequest.loginAttemptStarted = true;
+                browser.executeScript({ code: enterCredentials }, function(result) {});
+            }
+        },
+
+        /*
+        3.2 Although there already was a login attempt, we are taken to the login mask. There could be a technical problem, but we assume the login data was invalid. We have to listen for loadstop because posting the login data the first time results in a loadstart on the IdP url
+         */
+        loginFailed: {
+            type: "loadstop",
+            predicate: function(ev, loginRequest) { return ev.url === idpUrl && loginRequest.loginAttemptStarted; },
+            action: function(ev, loginRequest) {
+                loginRequest.error(ERROR_CREDENTIALS);
+            }
+        },
+
+        /*
+        Something went wrong. Propagate error. Only exception: Moodle token. We can't load that but we already handle it
+         */
+        technicalError: {
+            type: "loaderror",
+            predicate: function(ev) { return ev.url.indexOf(tokenUrl) === -1 && ev.url.indexOf("http://" + tokenUrl) === -1; },
+            action: function(ev, loginRequest) {
+                console.log("loaderror happened on " + ev.url);
+                loginRequest.error(ERROR_TECHNICAL);
             }
         }
     };
@@ -91,7 +121,7 @@ define([
     var handle = function(actions, loginRequest, event) {
         _.chain(actions)
             .filter(function(action) { return event.type === action.type })
-            .filter(function(action) { return action.predicate(event); })
+            .filter(function(action) { return action.predicate(event, loginRequest); })
             .each(function(action) { action.action(event, loginRequest, loginRequest.browser); });
     };
 
@@ -101,24 +131,46 @@ define([
     var idpUrl = "https://idp.uni-potsdam.de/idp/Authn/UserPassword";
     var tokenUrl = "moodlemobile://token=";
 
-    var openBrowser = function() {
+    var openBrowser = function(success, error) {
         var loginRequest = {
             session: new Session,
-            browser: window.open(pluginUrl, "_blank", "")
+            browser: window.open(pluginUrl, "_blank", "clearcache=yes,clearsessioncache=yes,hidden=yes")
+        };
+        var browser = loginRequest.browser;
+
+        var handleEvent = function(event) {
+            handle(actions, loginRequest, event);
         };
 
-        var browser = loginRequest.browser;
-        browser.addEventListener("loadstart", function(event) {
-            handle(actions, loginRequest, event);
-        });
+        var freeBrowser = function() {
+            browser.removeEventListener("loadstart", handleEvent);
+            browser.removeEventListener("loadstop", handleEvent);
+            browser.removeEventListener("loaderror", handleEvent);
+            browser.removeEventListener("exit", handleEvent);
+            browser.close();
+        };
 
-        browser.addEventListener("loadstop", function(event) {
-            handle(actions, loginRequest, event);
-        });
+        browser.addEventListener("loadstart", handleEvent);
+        browser.addEventListener("loadstop", handleEvent);
+        browser.addEventListener("loaderror", handleEvent);
+        browser.addEventListener("exit", handleEvent);
+
+        loginRequest.success = function() {
+            console.log("Success called");
+            freeBrowser();
+            success();
+        };
+        loginRequest.error = function() {
+            console.log("Error called");
+            freeBrowser();
+            error();
+        }
     };
 
-    var createToken = function(options) {
-        openBrowser();
+    var createToken = function() {
+        var promise = $.Deferred();
+        openBrowser(promise.resolve, promise.reject);
+        return promise.promise();
     };
 
     return {
