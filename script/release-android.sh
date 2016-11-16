174ee9c20dcf4a6c06bb0abb5b0bbd793b820a05
Hendrik Geßner <HENGE01@gmail.com>
2016-11-16 15:34:00 +0100

    Updated release and signing script.

--- script/release-android.sh
+++ script/release-android.sh
@@ -1,4 +1,11 @@
-cd www
-
 # Needs requirejs optimizer. See http://requirejs.org/docs/optimization.html
+cd www
 r.js -o baseUrl=js mainConfigFile=js/main.js name=main out=main-built.js
+cd ..
+
+cd platforms
+mkdir android
+cp release-signing.properties android
+cd ..
+
+cordova build android --release
