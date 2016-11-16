2f0f202b8cc2f4e68e04336a068e8a96ab140dab
Hendrik Geßner <HENGE01@gmail.com>
2016-11-16 12:35:42 +0100

    Updated fullbuild script and added optional parameter for build action (build or run).

--- script/fullbuild.sh
+++ script/fullbuild.sh
@@ -1,6 +1,19 @@
+# Usage: sh script\fullbuild.sh <platform> <action (optional)>
+# <platform>: ios or android
+# <action>: run (default) or build
+# e.g.
+#   sh script\fullbuild.sh ios build
+# or
+#   sh script\fullbuild.sh android
+
 if [ -z $1 ]; then
   echo "Need platform, e.g. android or ios";
   exit 1;
 fi
 
-cd platforms && rm -rf $1 && cd .. && rm -rf plugins && cordova prepare $1 && cordova run $1
+action=$2;
+if [ -z $2 ]; then
+  action="run";
+fi
+
+cd platforms && rm -rf $1 && cd .. && rm -rf plugins && cordova prepare $1 && cordova $action $1
