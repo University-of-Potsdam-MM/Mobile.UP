a2242dee0ed3c798b746f6379d78e9e70c035566
Ludwig Kräh <ludwig.kraeh@uni-potsdam.de>
2017-12-11 15:37:42 +0100

    (#522) Now tooltip is hidden as well when the countdown is active.

--- www/js/pmodules/options/options.js
+++ www/js/pmodules/options/options.js
@@ -172,11 +172,12 @@ define([
 				// Makes the login prompt invisble and thus not reachable if
 				// timer is running
 				this.$("#loginform_container").css('display', 'none');
+				this.$("#tooltip").css('display', 'none');
 			} else {
 				this.$("#error3").css('display', 'none');
 				// Makes the login prompt visible if timer is not running
 				this.$("#loginform_container").css('display', 'block');
-				
+				this.$("#tooltip").css('display', 'block');				
 			}
 			this.loadingView = new utils.LoadingView({model: this.model, el: this.$("#loadingSpinner")});
 
