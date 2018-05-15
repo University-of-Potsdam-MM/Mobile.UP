ddc7ae2f1d0fca330b2aa4eae5c9747d86b41959
Ludwig Kräh <ludwig.kraeh@uni-potsdam.de>
2018-05-15 12:37:02 +0200

    removed import

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { TranslateService } from "@ngx-translate/core";
-import {UPLoginProvider} from "../../providers/login-provider/login";
 import {Storage} from "@ionic/storage";
 
 /**
