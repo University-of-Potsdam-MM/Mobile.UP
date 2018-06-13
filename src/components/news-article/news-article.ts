59944827485d280f3d602b68062e0c013f6d0e6e
Lewin Kästner <lewin.kaestner@uni-potsdam.de>
2018-06-13 13:51:55 +0200

    open website if article-card is clicked

--- src/components/news-article/news-article.ts
+++ src/components/news-article/news-article.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { AlertController } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'news-article',
@@ -8,7 +11,34 @@ export class NewsArticleComponent {
 
   @Input() public article;
 
-  constructor() {
+  constructor(private iap: InAppBrowser, private alertCtrl: AlertController, private translate: TranslateService) {
+  }
+
+  openWebsite(link) {
+
+    let alert = this.alertCtrl.create({
+      title: this.translate.instant("alert.title.website"),
+      buttons: [
+        {
+          text: this.translate.instant("button.no"),
+          role: 'disagree',
+          handler: () => {
+            console.log("Dialog dismissed");
+          }
+        },
+        {
+          text: this.translate.instant("button.yes"),
+          role: 'agree',
+          handler: () => {
+            this.iap.create(link);
+          }
+        }
+      ],
+      enableBackdropDismiss: false,
+    });
+    alert.present();
+
+
   }
 
 }
