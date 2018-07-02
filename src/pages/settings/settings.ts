356612bf84e3b1c0ce77696474c6b235068137a1
vabene1111 <vabene1234@googlemail.com>
2018-07-02 19:14:52 +0200

    added setting config

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -4,6 +4,7 @@ import {Storage} from "@ionic/storage";
 import {ESettingType, ISetting, ISettingOption} from "../../library/interfaces";
 import {AlertController} from 'ionic-angular';
 import {TranslateService} from "@ngx-translate/core";
+import * as Constants from './setting_config';
 
 @IonicPage()
 @Component({
@@ -26,30 +27,7 @@ export class SettingsPage {
     console.log('ionViewDidLoad SettingsPage');
 
     //register settings for loading
-    this.settings.push({key: "test_ph", value: false, type: ESettingType.placeholder});
-    this.settings.push({key: "test_1", value: "", type: ESettingType.string, options: []});
-    this.settings.push({key: "test_2", value: 0, type: ESettingType.number, options: []});
-    this.settings.push({key: "test_3", value: false, type: ESettingType.boolean, options: []});
-    this.settings.push({key: "test_ph", value: false, type: ESettingType.placeholder});
-    this.settings.push({
-      key: "test_4",
-      value: "0",
-      options: [{key: 0, lbl: "deaktivated"}, {key: 1, lbl: "1"}, {key: 2, lbl: "2"}],
-      type: ESettingType.number_radio
-    });
-
-    this.settings.push({
-      key: "test_5",
-      value: "test 1",
-      options: [{key: "test_1", lbl: "Test 1"}, {key: "test_2", lbl: "Test 2"}, {key: "test_3", lbl: "Test 3"}],
-      type: ESettingType.string_radio
-    });
-    this.settings.push({
-      key: "test_6",
-      value: "test_1",
-      options: [{key: "test_1", lbl: "Test 1"}, {key: "test_2", lbl: "Test 2"}, {key: "test_3", lbl: "Test 3"}],
-      type: ESettingType.checkbox
-    });
+    this.settings = Constants.SETTINGS;
 
     for (let i = 0; i < this.settings.length; i++) {
       this.translate.get("page.settings.setting." + this.settings[i].key).subscribe(
