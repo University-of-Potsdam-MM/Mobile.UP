e5cff0c3a3cc1c21b354fd45aea92eefdcca3062
Lewin <33331864+lkstnr@users.noreply.github.com>
2018-10-12 14:03:24 +0200

    just show event location with at least 1 event

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -36,8 +36,6 @@ export class EventsPage {
 
   async ngOnInit() {
 
-    this.todaysLocationsList.push(this.translate.instant("page.events.noEventCategory"));
-
     let config: IConfig = await this.storage.get("config");
 
     let headers: HttpHeaders = new HttpHeaders()
@@ -47,7 +45,6 @@ export class EventsPage {
     this.http.get(url, {headers:headers}).subscribe((response:INewsApiResponse) => {
       if (response.errors.exist == false) {
         this.eventsList = response.vars.events;
-
         var i;
         for (var loc in response.vars.places) {
           for (i = 0; i < this.eventsList.length; i++) {
@@ -55,6 +52,15 @@ export class EventsPage {
             // hide locations with zero events
             if (response.vars.places[loc] == this.eventsList[i].Place.name) {
               this.locationsList.push(response.vars.places[loc]);
+
+              var timeBegin = moment(this.eventsList[i].Event.startTime * 1000);
+              var currentTime = moment();
+              var isToday = currentTime.isSame(timeBegin, "day");
+
+              if (isToday) {
+                this.todaysLocationsList.push(response.vars.places[loc]);
+              }
+
               break;
             }
           }
@@ -63,44 +69,12 @@ export class EventsPage {
           this.checkTodaysEvents(this.eventsList[i]);
         }
         this.checkNextEvents();
-        this.initSeperators();
         this.isLoaded = true;
       }
     })
 
   }
 
-  initSeperators() {
-    var i,j;
-    var tmpCounter = [];
-    for (i = 0; i < this.todaysLocationsList.length; i++) {
-      tmpCounter[i] = 0;
-      for (j = 0; j < this.todaysEventsList.length; j++) {
-        if (this.todaysLocationsList[i] == this.todaysEventsList[j].Place.name) {
-          if (tmpCounter[i] == 0) {
-            tmpCounter [i] = 1;
-            this.firstEventTodayForLocation[j] = true;
-          } else {
-            this.firstEventTodayForLocation[j] = false;
-          }
-        }
-      }
-    }
-    
-    for (i = 0; i < this.locationsList.length; i++) {
-      tmpCounter[i] = 0;
-      for (j = 0; j < this.nextEventsList.length; j++) {
-        if (this.locationsList[i] == this.nextEventsList[j].Place.name) {
-          if (tmpCounter[i] == 0) {
-            tmpCounter[i] = 1;
-            this.firstEventForLocation[j] = true;
-          } else {
-            this.firstEventForLocation[j] = false;
-          }
-        }
-      }
-    }
-  }
 
   checkTodaysEvents(event) {
     var timeBegin = moment(event.Event.startTime * 1000);
@@ -110,23 +84,6 @@ export class EventsPage {
     if (isToday) {
       this.todaysEventsList.push(event);
       this.eventsToday = true;
-
-      var i;
-      var alreadyAdded = false;
-
-      for (i = 0; i < this.todaysLocationsList.length; i++) {
-        if (this.todaysLocationsList[0] == this.translate.instant("page.events.noEventCategory")) {
-          this.todaysLocationsList[0] = event.Place.name;
-          alreadyAdded = true;
-        } else if (this.todaysLocationsList[i] == event.Place.name) {
-          alreadyAdded = true;
-        }
-      }
-
-      if (!alreadyAdded) {
-        this.todaysLocationsList.push(event.Place.name);
-      }
-
     }
   }
 
