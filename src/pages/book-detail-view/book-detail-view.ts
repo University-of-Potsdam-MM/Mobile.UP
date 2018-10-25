44410c9a87ccb6e2a2f5bac89db23af1c3a35632
Lewin <33331864+lkstnr@users.noreply.github.com>
2018-10-25 19:52:27 +0200

    added location view

--- src/pages/book-detail-view/book-detail-view.ts
+++ src/pages/book-detail-view/book-detail-view.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Item } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { IConfig } from '../../library/interfaces';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { SafariViewController } from '@ionic-native/safari-view-controller';
 
 @IonicPage()
 @Component({
@@ -16,14 +18,22 @@ export class BookDetailViewPage {
   activeSegment = "location";
   config:IConfig;
 
-  bookLocation;
+  bookLocationList;
   mediaType;
+  showLocation = true;
+  showDetails = false;
 
   bookDetails = {
     "url": null
   };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage, private http: HttpClient) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage,
+    private http: HttpClient,
+    private theInAppBrowser: InAppBrowser,
+    private platform: Platform,
+    private safari: SafariViewController) {
     this.book = this.navParams.data["book"];
     console.log(this.book);
   }
@@ -46,60 +56,94 @@ export class BookDetailViewPage {
   updateLocation() {
     let url = this.config.webservices.endpoint.libraryDAIA + this.book.recordInfo.recordIdentifier._ + "&format=json";
     this.http.get(url).subscribe(data => {
-      var locationModel = {
-        department:     this.getDepartment(data),
-        departmentURL:  this.getDepartmentURL(data),
-        label:          this.getLabel(data),
-        item:           this.getItem(data),
-        url:            this.getBookUrl(data)
-      }
-      this.bookLocation = locationModel;
+      this.setLocationData(data);
     }, error => {
       console.log(error);
-    })
+    });
   }
 
-  getDepartment(data) {
-    var department = "";
-    if (data.department && data.department.content) {
-      department = data.department.content;
+  setLocationData(data) {
+    console.log(data);
+    this.bookLocationList = [];
+    if (data) {
+      var i, j;
+      for (i = 0; i < data.document.length; i++) {
+        for (j = 0; j < data.document[i].item.length; j ++) {
+          var locationModel = {
+            department:     this.getDepartment(data.document[i].item[j]),
+            departmentURL:  this.getDepartmentURL(data.document[i].item[j]),
+            label:          this.getLabel(data.document[i].item[j]),
+            item:           this.getItem(data.document[i].item[j]),
+            url:            this.getBookUrl(data.document[i].item[j])
+          }
+          this.bookLocationList.push(locationModel);
+        }
+      }
     }
-    if (data.storage) {
-      department = department + ", " + data.storage.content;
+    console.log(this.bookLocationList);
+  }
+
+  getDepartment(item) {
+    var department = "";
+    if (item.department && item.department.content) {
+      department = item.department.content;
+    }
+    if (item.storage) {
+      department = department + ", " + item.storage.content;
     }
     return department;
   }
 
-  getDepartmentURL(data) {
-    if (data.department && data.department.id) {
-      return data.department.id;
+  getDepartmentURL(item) {
+    if (item.department && item.department.id) {
+      return item.department.id;
     } else { return ""; }
   }
 
-  getLabel(data) {
-    if (data.label) {
-      return data.label
+  getLabel(item) {
+    if (item.label) {
+      return item.label
     } else { return ""; }
   }
 
-  getBookUrl(data) {
+  getBookUrl(item) {
+
+    if (this.book.location) {
+      var i; 
+      let tmp = this.convertToArray(this.book.location);
+      for (i = 0; i < tmp.length; i++) {
+        if (tmp[i].url) {
+          var tmpUrl = this.convertToArray(tmp[i].url);
+          var j;
+          for (j = 0; j < tmpUrl.length; j++) {
+            if (tmpUrl[j].$ && (tmpUrl[j].$.usage == 'primary display')) {
+              if (tmpUrl[j]._) {
+                this.bookDetails.url = tmpUrl[j]._;
+              }
+            }
+          }
+        }
+      }
+    }
+
     if (this.mediaType == "mediatype_o") {
       var url;
-      if (data.unavailable && data.unavailable[0].service == "openaccess") {
-        url = data.unavailable[0].href;
+      if (item.unavailable && item.unavailable[0].service == "openaccess") {
+        url = item.unavailable[0].href;
       } else { url = null; }
       if (url != null) { this.bookDetails.url = url; }
-      return url;
     }
+
+    return this.bookDetails.url;
   }
 
-  getItem(data) {
+  getItem(item) {
     var status = "", statusInfo = "";
 
     // check for available / unavailable items and process loan and presentation
-    if (data.available) {
+    if (item.available) {
       var loanAvailable, presentationAvailable;
-      let availableArray = this.convertToArray(data.available);
+      let availableArray = this.convertToArray(item.available);
       var i;
       for (i = 0; i < availableArray.length; i++) {
         if (availableArray[i].service == "loan") {
@@ -111,9 +155,9 @@ export class BookDetailViewPage {
       }
     }
 
-    if (data.unavailable) {
+    if (item.unavailable) {
       var loanUnavailable, presentationUnavailable;
-      let unavailableArray = this.convertToArray(data.available);
+      let unavailableArray = this.convertToArray(item.available);
       var j;
       for (j = 0; j < unavailableArray.length; j++) {
         if (unavailableArray[j].service == "loan") {
@@ -141,13 +185,13 @@ export class BookDetailViewPage {
           status = "ausleihbar";
         } else { status = "nicht ausleihbar"; }
       } else {
-        if (this.bookDetails.url == null) {
-          if (data.label && data.label.indexOf("bestellt") != -1) {
-            status = data.label;
+        if (this.getBookUrl(item) == null) {
+          if (item.label && item.label.indexOf("bestellt") != -1) {
+            status = item.label;
             statusInfo = "";
           } else {
             status = "Präsenzbestand";
-            if (presentationAvailable.href) {
+            if (presentationAvailable && presentationAvailable.href) {
               statusInfo = presentationAvailable.href;
             }
           }
@@ -157,7 +201,7 @@ export class BookDetailViewPage {
       }
 
       if (presentationUnavailable) {
-        if (loanUnavailable.href) {
+        if (loanUnavailable && loanUnavailable.href) {
           if (loanUnavailable.href.indexOf("loan/RES") != -1) {
             status = "ausgeliehen";
             if(!loanUnavailable.expected || loanUnavailable.expected == "unknown") {
@@ -180,5 +224,28 @@ export class BookDetailViewPage {
   setMediaType(mediatype) {
     this.mediaType = mediatype;
   }
+
+  openWithInAppBrowser(url:string) {
+    let target = "_blank";
+    this.theInAppBrowser.create(url,target);
+  }
+
+  openWithSafari(url:string) {
+    this.safari.show({
+      url: url
+    }).subscribe(result => {console.log(result);}, error => { console.log(error); })
+  }
+
+  openWebsite(url:string) {
+    if (this.platform.is("ios") || this.platform.is("android")) {
+      this.safari.isAvailable().then((available:boolean) => {
+        if (available) {
+          this.openWithSafari(url);
+        } else { this.openWithInAppBrowser(url); }
+      });
+    } else {
+      this.openWithInAppBrowser(url);
+    }
+  } 
 
 }
