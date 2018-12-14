edf96e96f7068faba308cd7d666d6cfe6b6f65ec
Alexander Kiy <alekiy@uni-potsdam.de>
2018-12-14 14:11:19 +0100

    dont use components for popover integration

--- src/components/popover-button/popover-button.ts
+++ src/components/popover-button/popover-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import {IOIDCUserInformationResponse} from "../../providers/login-provider/interfaces";
 import {MorePopoverComponent} from "../more-popover/more-popover";
 import {PopoverController} from "ionic-angular";
@@ -16,8 +16,22 @@ import {Storage} from "@ionic/storage";
 })
 export class PopoverButtonComponent {
 
-  constructor(private storage: Storage,
+  constructor(private el: ElementRef,
+              private storage: Storage,
               private popoverCtrl: PopoverController) {}
+
+//wait for the component to render completely
+ngOnInit() {
+  var nativeElement: HTMLElement = this.el.nativeElement,
+      parentElement: HTMLElement = nativeElement.parentElement;
+  // move all children out of the element
+  while (nativeElement.firstChild) {
+      parentElement.insertBefore(nativeElement.firstChild, nativeElement);
+  }
+  // remove the empty element(the host)
+  parentElement.removeChild(nativeElement);
+}
+
 
   /**
    * presents popover
