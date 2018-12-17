0430c60530e44d69e7307b80a1bedee1ffa20840
Alexander Kiy <alekiy@uni-potsdam.de>
2018-12-17 09:32:51 +0100

    added settings for practice page

--- src/pages/settings/setting_config.ts
+++ src/pages/settings/setting_config.ts
@@ -4,6 +4,10 @@ import { ESettingType } from "../../library/interfaces";
 
 export const SETTINGS = [
     {
+        key: "general",
+        type: ESettingType.placeholder
+    },
+    {
         key: "language",
         value: "Deutsch",
         options: [{key: "Deutsch"}, {key: "Englisch"}],
@@ -16,5 +20,46 @@ export const SETTINGS = [
         options: [{key: "Neues Palais"}, {key: "Griebnitzsee"}, {key: "Golm"}],
         type: ESettingType.string_radio,
         icon: "ios-locate-outline"
+    },
+    {
+        key: "inpractice",
+        type: ESettingType.placeholder
+    },
+    {
+        key: "practice",
+        value: "Jobs für Studierende, Jobs für Absolventen, Abschlussarbeit, Praktika",
+        options: [{key: "Jobs für Studierende"}, {key: "Jobs für Absolventen"}, {key: "Abschlussarbeit"}, {key: "Praktika"}],
+        type: ESettingType.checkbox,
+        icon: "ios-filing-outline"
+    },
+    {
+        key: "studyarea",
+        value: "",
+        options: [
+            {key: "Geistes- und Kulturwissenschaften"},
+            {key: "Wirtschafts- und Sozialwissenschaften"},
+            {key: "Mathematik und Informatik"},
+            {key: "Naturwissenschaften"},
+            {key: "Rechtswissenschaften"},
+            {key: "Bildungs- und Kognitionswissenschaften"},
+            {key: "Sport- und Gesundheitswissenschaften"},
+            {key: "fächerübergreifend"},
+        ],
+        type: ESettingType.checkbox,
+        icon: "ios-filing-outline"
+    },
+    {
+        key: "domestic",
+        value: true,
+        type: ESettingType.boolean,
+        icon: "ios-plane-outline",
+        options: []
+    },
+    {
+        key: "foreign",
+        value: true,
+        type: ESettingType.boolean,
+        icon: "ios-plane-outline",
+        options: []
     }
 ];
