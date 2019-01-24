0fb364c28d477584729f010927fe90095541794c
Lewin Kästner <lewin.kaestner@uni-potsdam.de>
2019-01-24 16:25:54 +0100

    "Leistungsübersicht ohne Noten?": Anpassung an alte App

--- src/components/grades-table/grades-table.ts
+++ src/components/grades-table/grades-table.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 export class GradesTableComponent {
 
   @Input() studentGrades;
+  @Input() studentDetails;
   @Input() isDualDegree;
   @Input() i;
 
