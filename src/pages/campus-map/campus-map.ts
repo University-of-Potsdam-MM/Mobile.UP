b612cbf29f2c77023e7cc20c97b32bd08b199e6e
Alexander Kiy <alekiy@uni-potsdam.de>
2019-01-31 09:40:01 +0100

    handling async loading of page and using campusbar ref #8

--- src/pages/campus-map/campus-map.ts
+++ src/pages/campus-map/campus-map.ts
@@ -1,18 +1,19 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
-import {ICampus, IConfig} from "../../library/interfaces";
-import {ConfigProvider} from "../../providers/config/config";
+import { ICampus, IConfig } from "../../library/interfaces";
+import { ConfigProvider } from "../../providers/config/config";
 import * as leaflet from 'leaflet';
-import {IMapsResponse, IMapsResponseObject} from "../../library/IGeoJson";
-import {SettingsProvider} from "../../providers/settings/settings";
-import {WebServiceProvider} from "../../providers/web-service/web-service";
+import { IMapsResponse, IMapsResponseObject } from "../../library/IGeoJson";
+import { SettingsProvider } from "../../providers/settings/settings";
+import { ConnectionProvider } from "../../providers/connection/connection";
+import { WebServiceProvider } from "../../providers/web-service/web-service";
 
 @IonicPage()
 @Component({
   selector: 'page-campus-map',
   templateUrl: 'campus-map.html',
 })
-export class CampusMapPage implements AfterViewInit{
+export class CampusMapPage {
 
   query:string;
 
@@ -21,13 +22,142 @@ export class CampusMapPage implements AfterViewInit{
   selectedCampus:ICampus;
   categories:string[] = [];
   layers:{[name:string]:leaflet.GeoJSON} = {};
+  campus;
 
   @ViewChild('map') mapContainer: ElementRef;
   map: any;
 
-  constructor(private settings:SettingsProvider,
-              private wsProvider: WebServiceProvider) {
+  constructor(
+    private settings:SettingsProvider,
+    private connection: ConnectionProvider,
+    private wsProvider: WebServiceProvider) {
   }
+
+  /**
+   * @name ionViewWillEnter
+   * @async
+   * @description take user to login if there is no session.
+   * We are using ionViewDidEnter here because it is run every time the view is
+   * entered, other than ionViewDidLoad which will run only once
+   */
+  async ionViewWillEnter() {
+    this.connection.checkOnline(true, true);
+    // initialize map
+    this.loadMap();
+  }
+
+
+  public async changeCampus(campus) {
+
+    this.campus = campus;
+    console.log(this.campus)
+    this.loadCampusMap();
+  }
+
+  /**
+   * @async
+   * @description load campus map
+   */
+  async loadCampusMap() {
+
+    // select default campus first as specified in config in config
+    this.selectCampus(this.getDefaultCampus());
+
+    // fit bounds to show selected campus
+    this.moveToCampus(this.selectedCampus);
+
+    // set up scheduled geoJson retrieval
+    this.wsProvider.getMapData().subscribe(
+      (response:IMapsResponse) => {
+        this.geoJSON = response;
+        this.addFeatures()
+      },
+      error => {
+        console.log(error)
+      }
+    )
+
+  }
+
+
+  /**
+   * @name loadMap
+   * @description loads map and initializes it
+   */
+  loadMap(){
+    // create map object
+    this.map = leaflet.map("map");
+    leaflet.tileLayer(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'www.uni-potsdam.de',
+        maxZoom: 18
+      }).addTo(this.map);
+  }
+
+
+  /**
+   * @name getDefaultCampus
+   * @description Returns the default campus object based on the information
+   * provided in config.general.location
+   * @return {ICampus} default campus object
+   */
+  getDefaultCampus():ICampus{
+    return this.config.campusmap.campi.filter(
+      (campus:ICampus) => {
+        // special logic to map NeuesPalais == Neues Palais and so on
+        return this.campus == campus.pretty_name.replace(/\s+/g, '');
+      }
+    )[0]
+  }
+
+
+  /**
+   * @name selectCampus
+   * @description selects the given campus
+   * @param {ICampus} campus
+   */
+  selectCampus(campus:ICampus){
+    this.selectedCampus = campus;
+    if(this.map){
+      this.moveToCampus(this.selectedCampus)
+    }
+  }
+
+
+  /**
+   * @name moveToCampus
+   * @description fits map to given campus
+   * @param {ICampus} campus
+   */
+  moveToCampus(campus:ICampus){
+    this.map.fitBounds(
+      campus.lat_long_bounds
+    );
+  }
+
+
+  /**
+   * @name addFeatures
+   * @description collects categories from geoJson and returns array of unique
+   * categories
+   */
+  addFeatures(){
+    console.log(this.geoJSON);
+    this.geoJSON.forEach(
+      (obj:IMapsResponseObject) => {
+        // Create new layer for each new category
+        if(this.categories.indexOf(obj.category) == -1){
+          this.categories.push(obj.category);
+          this.layers[obj.category] = leaflet.geoJSON().addTo(this.map)
+        }
+        // add features from each category to corresponding layer
+        for(let feature of obj.geo.features){
+          this.layers[obj.category].addData(<any>feature)
+        }
+      }
+    );
+  }
+
 
   /**
    * @name search
@@ -50,114 +180,14 @@ export class CampusMapPage implements AfterViewInit{
     }
   }
 
-  /**
-   * @name getGeoJson
-   * @description Retrieves map data from server
-   */
-  loadGeoJson(){
-    this.wsProvider.getMapData().subscribe(
-      (response:IMapsResponse) => {
-        this.geoJSON = response;
-      },
-      error => {
-        console.log(error)
-      }
-    )
-  }
 
-  /**
-   * @name addFeatures
-   * @description collects categories from geoJson and returns array of unique
-   * categories
-   */
-  addFeatures(){
-    this.geoJSON.forEach(
-      (obj:IMapsResponseObject) => {
-        // Create new layer for each new category
-        if(this.categories.indexOf(obj.category) == -1){
-          this.categories.push(obj.category);
-          this.layers[obj.category] = leaflet.geoJSON().addTo(this.map)
-        }
-        // add features from each category to corresponding layer
-        for(let feature of obj.geo.features){
-          this.layers[obj.category].addData(<any>feature)
-        }
-      }
-    );
-  }
 
-  /**
-   * @name getDefaultCampus
-   * @description Returns the default campus object based on the information
-   * provided in config.general.location
-   * @return {ICampus} default campus object
-   */
-  getDefaultCampus():ICampus{
-    return this.config.campusmap.campi.filter(
-      (campus:ICampus) => {
-        this.settings.getSettingValue("campus");
-        // TODO
-        // return campus.id == this.config.general.location.campus
-      }
-    )[0]
-  }
 
-  /**
-   * @name selectCampus
-   * @description selects the given campus
-   * @param {ICampus} campus to select
-   */
-  selectCampus(campus:ICampus){
-    this.selectedCampus = campus;
-    if(this.map){
-      this.moveToCampus(this.selectedCampus)
-    }
-  }
 
-  /**
-   * @name moveToCampus
-   * @description fits map to given campus
-   * @param coordinates
-   */
-  moveToCampus(campus:ICampus){
-    this.map.fitBounds(
-      campus.lat_long_bounds
-    );
-  }
 
-  /**
-   * @name loadMap
-   * @description loads map and initializes it
-   */
-  loadMap(){
-    // create map object
-    this.map = leaflet.map("map");
-    leaflet.tileLayer(
-      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'www.uni-potsdam.de',
-        maxZoom: 18
-      }).addTo(this.map);
-  }
 
-  /**
-   * @name ngAfterViewInit
-   * @description triggers methods for initializing this component
-   */
-  ngAfterViewInit (){
-    // initialize map
-    this.loadMap();
 
-    // select default campus first as specified in config in config
-    this.selectCampus(this.getDefaultCampus());
 
-    // fit bounds to show selected campus
-    this.moveToCampus(this.selectedCampus);
 
-    // set up scheduled geoJson retrieval
-    this.loadGeoJson();
-
-    // reads categories from geoJson and adds features of each category
-    this.addFeatures()
-  }
 
 }
