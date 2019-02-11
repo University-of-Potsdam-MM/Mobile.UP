79903e93fe128aa2f677da8bd14ad24ccc214ca2
Alexander Kiy <alekiy@uni-potsdam.de>
2019-02-11 17:38:25 +0100

    improved code due to codeclimate

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, LoadingController, Loading, NavController, Events } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
-import { UPLoginProvider } from "../../providers/login-provider/login";
-import { ELoginErrors, ICredentials, IOIDCUserInformationResponse, ISession } from "../../providers/login-provider/interfaces";
-import { TranslateService } from "@ngx-translate/core";
-import { Observable } from "rxjs/Observable";
+import { IonicPage, LoadingController, Loading, NavController, Events, AlertController } from 'ionic-angular';
+import { UPLoginProvider } from '../../providers/login-provider/login';
+import { ELoginErrors, ICredentials, IOIDCUserInformationResponse, ISession } from '../../providers/login-provider/interfaces';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
 import { HomePage } from '../home/home';
-import { ConnectionProvider } from "../../providers/connection/connection";
-import { ConfigProvider } from "../../providers/config/config";
+import { ConnectionProvider } from '../../providers/connection/connection';
+import { ConfigProvider } from '../../providers/config/config';
 import { SessionProvider } from '../../providers/session/session';
 
 /**
@@ -27,25 +26,42 @@ export class LoginPage {
   alreadyLoggedIn: boolean;
 
   // This object will hold the data the user enters in the login form
-  loginCredentials:ICredentials = {
+  loginCredentials: ICredentials = {
     username: '',
     password: ''
   };
 
+  /**
+   * @constructor
+   * @param navCtrl
+   * @param loadingCtrl
+   * @param alertCtrl
+   * @param upLogin
+   * @param events
+   * @param connection
+   * @param sessionProvider
+   * @param translate
+   */
   constructor(
-      private navCtrl:     NavController,
+      private navCtrl: NavController,
       private loadingCtrl: LoadingController,
-      private alertCtrl:   AlertController,
-      private upLogin:     UPLoginProvider,
-      private events:      Events,
-      private connection:  ConnectionProvider,
+      private alertCtrl: AlertController,
+      private upLogin: UPLoginProvider,
+      private events: Events,
+      private connection: ConnectionProvider,
       private sessionProvider: SessionProvider,
-      private translate:   TranslateService) {
+      private translate: TranslateService) {
   }
 
+  /**
+   * @async
+   * @name ngOnInit
+   */
   async ngOnInit() {
+    this.connection.checkOnline(true, true);
+
     let tmp = await this.sessionProvider.getSession();
-    var session = undefined;
+    let session = undefined;
     if (tmp) {
       if (typeof tmp !== 'object') {
         session = JSON.parse(tmp);
@@ -55,17 +71,15 @@ export class LoginPage {
     if (session) {
       this.alreadyLoggedIn = true;
     } else { this.alreadyLoggedIn = false; }
-
-    this.connection.checkOnline(true, true);
   }
 
-  /**
-   * logins
-   *
-   * Uses AuthServiceProvider to execute login. If login is successful the user
+   /**
+   * @async
+   * @name login
+   * @description Uses AuthServiceProvider to execute login. If login is successful the user
    * is taken back to the previous page. If not, an alert is shown.
    */
-  public async login () {
+  public async login() {
 
     this.showLoading();
     let config = ConfigProvider.config;
@@ -79,7 +93,7 @@ export class LoginPage {
     if(session) {
       // now handle the Observable which hopefully contains a session
       session.subscribe(
-        (session:any) => {
+        (session: any) => {
           console.log(`[LoginPage]: Login successfully executed. Token: ${session.token}`);
           this.sessionProvider.setSession(session);
 
@@ -87,10 +101,10 @@ export class LoginPage {
 
           // in the meantime get user information and save it to storage
           this.upLogin.oidcGetUSerInformation(session, config.authorization.oidc).subscribe(
-            (userInformation:IOIDCUserInformationResponse) => {
+            (userInformation: IOIDCUserInformationResponse) => {
               this.sessionProvider.setUserInfo(userInformation);
             },
-            error => {
+            (error) => {
               // user must not know if something goes wrong here, so we don't
               // create an alert
               console.log(`[LoginPage]: Could not retrieve user information because:\n${JSON.stringify(error)}`);
@@ -98,26 +112,31 @@ export class LoginPage {
           );
 
           setTimeout(() => {
-            this.events.publish("userLogin");
+            this.events.publish('userLogin');
             this.navCtrl.pop();
           }, 1000);
         },
         error => {
           console.log(error);
           this.endLoading();
-          this.showAlert(error.reason)
+          this.showAlert(error.reason);
         }
       );
     } else {
       this.showAlert(ELoginErrors.UNKNOWN_ERROR);
-      console.log("[LoginPage]: Somehow no session has been passed by login-provider");
+      console.log('[LoginPage]: Somehow no session has been passed by login-provider');
     }
   }
 
-  autoCorrectUsername(loginCredentials:ICredentials) {
+
+  /**
+   * @name autoCorrectUsername
+   * @param {ICredentials} loginCredentials
+   */
+  autoCorrectUsername(loginCredentials: ICredentials) {
     // removes everything after (and including) @ in the username
     let foundAt = loginCredentials.username.indexOf("@");
-    if (foundAt != -1) {
+    if (foundAt !== -1) {
       loginCredentials.username = loginCredentials.username.substring(0, foundAt);
       this.loginCredentials.username = loginCredentials.username;
     }
@@ -125,46 +144,46 @@ export class LoginPage {
     return loginCredentials;
   }
 
+
   /**
-   * showLoading
-   *
-   * shows a loading animation
+   * @name showLoading
+   * @description shows a loading animation
    */
   private showLoading(): void {
     this.loading = this.loadingCtrl.create({
-      content: this.translate.instant("page.login.loginInProgress"),
+      content: this.translate.instant('page.login.loginInProgress'),
       dismissOnPageChange: true,
-      spinner: "crescent"
+      spinner: 'crescent'
     });
     this.loading.present();
   }
 
+
   /**
-   * endLoading
-   *
-   * ends the loading animation
+   * @name endLoading
+   * @description ends the loading animation
    */
   private endLoading(): void {
     this.loading.dismiss();
   }
 
+
   /**
-   * showAlert
-   *
-   * shows an alert
+   * @name showAlert
+   * @param errorCode
    */
   private showAlert(errorCode: ELoginErrors): void {
 
     let alert = this.alertCtrl.create({
-      title: this.translate.instant("alert.title.error"),
+      title: this.translate.instant('alert.title.error'),
       subTitle: this.translate.instant(`page.login.loginError.${errorCode}`),
-      buttons: [ this.translate.instant("button.continue") ]
+      buttons: [ this.translate.instant('button.continue') ]
     });
     alert.present();
   }
 
   public abort() {
-    this.navCtrl.setRoot(HomePage,{}, {animate: true, direction: "forward"});
+    this.navCtrl.setRoot(HomePage,{}, {animate: true, direction: 'forward'});
   }
 
 }
