c1ccbd2564925a8ee9eda65557a567fc785fe609
Alexander Kiy <alekiy@uni-potsdam.de>
2019-02-15 08:23:50 +0100

    fixed display of resourcetype and improved styling closes #667

--- src/components/book-list-view/book-list-view.ts
+++ src/components/book-list-view/book-list-view.ts
@@ -24,10 +24,13 @@ export class BookListViewComponent {
     this.getPublisher();
   }
 
+  /**
+   * @name getPublisher
+   */
   getPublisher() {
     if (this.book.relatedItem) {
       let tmp = utils.convertToArray(this.book.relatedItem);
-      var i;
+      let i;
       for (i = 0; i < tmp.length; i++) {
         if (tmp[i].originInfo && tmp[i].originInfo.publisher) {
           if (tmp[i].originInfo.publisher.trim().length > 0) {
@@ -39,93 +42,104 @@ export class BookListViewComponent {
     }
   }
 
+  /**
+   * @name checkMediaType
+   * @param typeOfResource
+   * @param originInfo
+   * @param physicalDescription
+   */
   checkMediaType(typeOfResource, originInfo, physicalDescription) {
-    var isRemote = false;
+
+    let physicalDescriptionArr = utils.convertToArray(physicalDescription.form);
     if (physicalDescription) {
-      var i;
-      var physicalDescriptionArr = utils.convertToArray(physicalDescription.form);
-      for (i = 0; i < physicalDescriptionArr.length; i++) {
-        if (physicalDescriptionArr[i]._ == "remote") {
-          isRemote = true;
-        }
-        if (physicalDescriptionArr[i]._ == "microform") {
-          this.mediaType.emit("mediatype_e");
-          this.bookDetails.icon = "help"; // E = ???
-          return;
-        }
-      }
+      physicalDescription = physicalDescriptionArr.filter(x => x._ == 'remote' || x._ == 'microform');
+      if (physicalDescription[0]){ physicalDescription = physicalDescription[0]._; }
+    }
+    if (typeOfResource._) { typeOfResource = typeOfResource._ }
+
+    if (physicalDescription == 'microform'){
+      this.mediaType.emit("mediatype_e");
     }
 
-    if (typeOfResource._) { typeOfResource = typeOfResource._ }
     let soundRec:boolean = typeOfResource.includes("sound recording");
+
+    //console.log(originInfo.issuance, physicalDescription, typeOfResource);
+
+    if (physicalDescription === 'remote'){
+      this.mediaType.emit("mediatype_o");
+      this.bookDetails.icon = "cloud-outline"; // O = Online
+      return;
+    }
+
     if (typeOfResource) {
       switch(typeOfResource) {
-        case "manuscript": {
-          this.mediaType.emit("mediatype_h");
-          this.bookDetails.icon = "help"; // H = ???
-          return;
+        case 'manuscript': {
+          this.mediaType.emit('mediatype_h');
+          this.bookDetails.icon = 'help'; // H = ???
+          break;
         }
-        case "still image": {
-          this.mediaType.emit("mediatype_i");
-          this.bookDetails.icon = "ios-image-outline"; // I = Image
-          return;
+        case 'still image': {
+          this.mediaType.emit('mediatype_i');
+          this.bookDetails.icon = 'ios-image-outline'; // I = Image
+          break;
         }
-        case "cartographic": {
-          this.mediaType.emit("mediatype_k");
-          this.bookDetails.icon = "ios-map-outline"; // K = Kartografie
-          return;
+        case 'cartographic': {
+          this.mediaType.emit('mediatype_k');
+          this.bookDetails.icon = 'ios-map-outline'; // K = Kartografie
+          break;
         }
-        case "notated music": {
-          this.mediaType.emit("mediatype_m");
-          this.bookDetails.icon = "ios-musical-notes-outline"; // M = Music
-          return;
+        case 'notated music': {
+          this.mediaType.emit('mediatype_m');
+          this.bookDetails.icon = 'ios-musical-notes-outline'; // M = Music
+          break;
         }
-        case "moving image": {
-          this.mediaType.emit("mediatype_v");
-          this.bookDetails.icon = "ios-videocam-outline"; // V = Video
-          return;
+        case 'moving image': {
+          this.mediaType.emit('mediatype_v');
+          this.bookDetails.icon = 'ios-videocam-outline'; // V = Video
+          break;
         }
-        case "text": {
-          if (originInfo && (originInfo.issuance == "serial" || originInfo.issuance == "continuing")) {
-            this.mediaType.emit("mediatype_t");
-            this.bookDetails.icon = "ios-document-outline"; // T = Text
-            return;
+        case 'text': {
+          if (originInfo && (originInfo.issuance === 'serial' || originInfo.issuance === 'continuing')) {
+            this.mediaType.emit('mediatype_t');
+            this.bookDetails.icon = 'ios-document-outline'; // T = Text
+            break;
           } else {
-            this.mediaType.emit("mediatype_b");
-            this.bookDetails.icon = "md-bookmarks"; // B = Book
-            return;
+            this.mediaType.emit('mediatype_b');
+            this.bookDetails.icon = 'md-bookmarks'; // B = Book
+            break;
           }
         }
-        case "software, multimedia": {
+        case 'software, multimedia': {
+          //console.log('software?');
           if (originInfo && (originInfo.issuance == "serial" || originInfo.issuance == "continuing")) {
-            if (isRemote) {
+            if (physicalDescription === 'remote') {
               this.mediaType.emit("mediatype_p");
               this.bookDetails.icon = "ios-paper-outline"; // P = Paper
-              return;
+              break;
             } else {
-              this.mediaType.emit("mediatype_t");
-              this.bookDetails.icon = "ios-document-outline"; // T = Text
-              return;
+              this.mediaType.emit('mediatype_t');
+              this.bookDetails.icon = 'ios-document-outline'; // T = Text
+              break;
             }
-          } else if (isRemote) {
-            this.mediaType.emit("mediatype_o");
-            this.bookDetails.icon = "cloud-outline"; // O = Online
-            return;
+          } else if (physicalDescription === 'remote') {
+            this.mediaType.emit('mediatype_o');
+            this.bookDetails.icon = 'cloud-outline'; // O = Online
+            break;
           } else {
-            this.mediaType.emit("mediatype_s");
-            this.bookDetails.icon = "ios-disc-outline"; // S = Software
-            return;
+            this.mediaType.emit('mediatype_s');
+            this.bookDetails.icon = 'ios-disc-outline'; // S = Software
+            break;
           }
         }
         default: {
           if (soundRec) {
-            this.mediaType.emit("mediatype_g");
-            this.bookDetails.icon = "volume-up"; // G = Recordings
-            return;
+            this.mediaType.emit('mediatype_g');
+            this.bookDetails.icon = 'volume-up'; // G = Recordings
+            break;
           } else {
-            this.mediaType.emit("mediatype_x");
-            this.bookDetails.icon = "help"; // X = undefined
-            return;
+            this.mediaType.emit('mediatype_x');
+            this.bookDetails.icon = 'help'; // X = undefined
+            break;
           }
         }
       }
@@ -136,7 +150,7 @@ export class BookListViewComponent {
   /**
    * has to be declared for html pages to use the imported function
    * couldn't find a better solution
-   * @param array 
+   * @param array
    */
   convertToArray(array) {
     return utils.convertToArray(array)
