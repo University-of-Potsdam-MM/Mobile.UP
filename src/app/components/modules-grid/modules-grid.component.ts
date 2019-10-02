bc191747f8ea7b1d2952e48283396879e9a6ac77
Ludwig Weihmann <ludwig.weihmann@uni-potsdam.de>
2019-10-02 18:04:45 +0200

    ref #635, works better now, layout is still ugly though

--- src/app/components/modules-grid/modules-grid.component.ts
+++ src/app/components/modules-grid/modules-grid.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {DisplayGrid, GridsterConfig} from 'angular-gridster2';
+import {DisplayGrid, GridsterConfig, GridsterItem} from 'angular-gridster2';
+import {IModule} from '../../lib/interfaces';
 
 @Component({
   selector: 'modules-grid',
@@ -8,31 +9,54 @@ import {DisplayGrid, GridsterConfig} from 'angular-gridster2';
 })
 export class ModulesGridComponent implements OnInit {
 
-  @Input() modules;
+  /**
+   * converts passed modules list to gridsterItems
+   * @param modules
+   */
+  @Input() set modules(modules: IModule[]) {
+    this.items = modules.map(
+      m => {
+        return {
+          x: undefined,
+          y: undefined,
+          rows: 1,
+          cols: 1,
+          module: m
+        };
+      }
+    );
+  }
+
+  /**
+   * input for the template to be used inside the gridsterItems.
+   * Inside the template "module" will be available as context
+   */
   @Input() template;
 
+  items: GridsterItem[] = [];
   options: GridsterConfig;
 
   constructor() {}
 
   ngOnInit() {
     this.options = {
+      gridType: 'fit',
       compactType: 'compactLeft&Up',
       defaultItemCols: 1,
       defaultItemRows: 1,
-      displayGrid: DisplayGrid.Always,
-      delayStart: 10,
+      minRows: 1,
+      maxRows: 10,
       minCols: 4,
       maxCols: 4,
-      minRows: 4,
-      maxRows: 4,
+      mobileBreakpoint: 1,
+      displayGrid: DisplayGrid.None,
+      delayStart: 10,
       draggable: {
         enabled: true
       },
       resizable: {
         enabled: false
-      },
-      gridType: 'fit'
+      }
     };
   }
 
