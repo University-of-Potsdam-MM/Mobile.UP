1af13931227fa44f1be2c5303d08d126d44dea46
Lewin Kästner <33331864+lkstnr@users.noreply.github.com>
2021-03-24 18:20:01 +0100

    fix: android plugin init and icons

--- android/app/src/main/java/de/floriangoessler/upapp/MainActivity.java
+++ android/app/src/main/java/de/floriangoessler/upapp/MainActivity.java
@@ -1,22 +1,12 @@
 package de.floriangoessler.upapp;
-
 import android.os.Bundle;
-import com.getcapacitor.Plugin;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.plugin.http.Http;
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Initializes the Bridge
-        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-            // Additional plugins you've installed go here
-            // Ex: add(TotallyAwesomePlugin.class);
-            add(Http.class);
-        }});
+        registerPlugin(Http.class);
     }
 }
-
