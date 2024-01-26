615f6f8f04d6f0b98fb7e771fcca48f1706afce3
Johannes Grobelski <grobelskijohannes@gmail.com>
2024-01-26 12:36:33 +0100

    837: fetch hinweistext

--- src/app/components/info-banner/info-banner.component.ts
+++ src/app/components/info-banner/info-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
+import axios from 'axios';
 
 const prefKey = 'MobileUP_DontShowBanner';
 
@@ -9,15 +10,41 @@ const prefKey = 'MobileUP_DontShowBanner';
   styleUrls: ['./info-banner.component.scss'],
 })
 export class InfoBannerComponent implements OnInit {
+  public title = 'Hinweis1';
+  public subtitle = 'Erklärung1';
+  public description = 'Beschreibung1';
+
   constructor() {}
 
   ngOnInit() {
     this.checkPref().then((prefValue) => {
       console.log('render info banner: ' + prefValue);
       if (prefValue === true) {
-        this.closeCard();
+        //this.closeCard();
       }
     });
+
+    //fetch values of Hinweistext
+    const fetchHinweisText = () => {
+      axios
+        .get(`http://n-apiprovider-01.rz.uni-potsdam.de:3000/api/hinweistext`)
+        .then((response) => {
+          const { title, subtitle, description } = response.data;
+          this.title = title;
+          this.subtitle = subtitle;
+          this.description = description;
+          if (title === '' && subtitle === '' && description === '') {
+            this.closeCard();
+          }
+        })
+        .catch((err) => {
+          this.title = this.subtitle = this.description = '';
+          this.closeCard();
+          console.error(err);
+        })
+        .finally(() => {});
+    };
+    fetchHinweisText();
   }
 
   closeCard(): void {
